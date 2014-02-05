f103549a05db6a6922d9b6e12e4f8ab100724b18
Hannes Kroeger <hannes@kroegeronline.net>
2014-02-05 08:56:17 +0100

    provide detection of OF16ext

---
+++ src/CMake/FindOpenFOAM-16ext.cmake
@@ -0,0 +1,47 @@
+# Try to find OpenFOAM-1.6-ext
+# Once done this will define
+#
+# OF16ext_FOUND          - system has OpenFOAM-1.6-ext installed
+
+
+
+FIND_PATH(OF16ext_DIR NAMES etc/bashrc
+  HINTS
+  $ENV{HOME}/OpenFOAM/OpenFOAM-1.6-ext
+  /opt/OpenFOAM/OpenFOAM-1.6-ext
+)
+message(STATUS ${OF16ext_DIR})
+
+SET(OF16ext_FOUND FALSE)
+
+IF(OF16ext_DIR)
+  set(OF16ext_BASHRC "${OF16ext_DIR}/etc/bashrc")
+
+  execute_process(COMMAND ${CMAKE_SOURCE_DIR}/CMake/getOFCfgVar ${OF16ext_BASHRC} print-c++FLAGS OUTPUT_VARIABLE OF16ext_CXX_FLAGS)
+  set(OF16ext_CXX_FLAGS "${OF16ext_CXX_FLAGS} -DOF16ext")
+
+  execute_process(COMMAND ${CMAKE_SOURCE_DIR}/CMake/getOFCfgVar ${OF16ext_BASHRC} print-WM_OPTIONS OUTPUT_VARIABLE OF16ext_WM_OPTIONS)
+  execute_process(COMMAND ${CMAKE_SOURCE_DIR}/CMake/getOFCfgVar ${OF16ext_BASHRC} print-METIS_LIB_DIR OUTPUT_VARIABLE OF16ext_METIS_LIB_DIR)
+  execute_process(COMMAND ${CMAKE_SOURCE_DIR}/CMake/getOFCfgVar ${OF16ext_BASHRC} print-PARMETIS_LIB_DIR OUTPUT_VARIABLE OF16ext_PARMETIS_LIB_DIR)
+  execute_process(COMMAND ${CMAKE_SOURCE_DIR}/CMake/getOFCfgVar ${OF16ext_BASHRC} print-SCOTCH_LIB_DIR OUTPUT_VARIABLE OF16ext_SCOTCH_LIB_DIR)
+
+  set(OF16ext_LIBSRC_DIR "${OF16ext_DIR}/src")
+  set(OF16ext_LIB_DIR "${OF16ext_DIR}/lib/${OF16ext_WM_OPTIONS}")
+  
+  execute_process(COMMAND ${CMAKE_SOURCE_DIR}/CMake/getOFCfgVar ${OF16ext_BASHRC} print-LINKLIBSO OUTPUT_VARIABLE OF16ext_LINKLIBSO)
+  execute_process(COMMAND ${CMAKE_SOURCE_DIR}/CMake/getOFCfgVar ${OF16ext_BASHRC} print-LINKEXE OUTPUT_VARIABLE OF16ext_LINKEXE)
+  execute_process(COMMAND ${CMAKE_SOURCE_DIR}/CMake/getOFCfgVar ${OF16ext_BASHRC} print-FOAM_MPI_LIBBIN OUTPUT_VARIABLE OF16ext_FOAM_MPI_LIBBIN)
+  
+  macro (setup_target_OF16ext targetname exename)
+    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${OF16ext_CXX_FLAGS}")
+    set(LINK_FLAGS "${LINK_FLAGS} ${OF16ext_LINKEXE}")
+    link_directories(${OF16ext_LIB_DIR} ${OF16ext_FOAM_MPI_LIBBIN} ${OF16ext_METIS_LIB_DIR} ${OF16ext_PARMETIS_LIB_DIR} ${OF16ext_SCOTCH_LIB_DIR})
+    add_executable(${targetname} ${${targetname}_SOURCES})
+    set_target_properties(${targetname} PROPERTIES OUTPUT_NAME ${exename})
+    set_target_properties(${targetname} PROPERTIES RUNTIME_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/bin/OpenFOAM-1.6-ext)
+    message(STATUS ${ARGN})
+    target_link_libraries(${targetname} ${ARGN}) 
+  endmacro()
+  
+  SET(OF16ext_FOUND TRUE)
+ENDIF(OF16ext_DIR)
--- src/CMake/FindOpenFOAM-22x.cmake
+++ src/CMake/FindOpenFOAM-22x.cmake
@@ -1,10 +1,8 @@
 # Try to find OpenFOAM-2.2.x
 # Once done this will define
 #
-# OF22x_FOUND          - system has OCC - OpenCASCADE
-# OF22x_INCLUDE_DIR    - where the OCC include directory can be found
-# OF22x_LIBRARY_DIR    - where the OCC library directory can be found
-# OF22x_LIBRARIES      - Link this to use OCC
+# OF22x_FOUND          - system has OpenFOAM-2.2.x installed
+
 
 
 FIND_PATH(OF22x_DIR NAMES etc/bashrc
@@ -12,35 +10,31 @@ FIND_PATH(OF22x_DIR NAMES etc/bashrc
   $ENV{HOME}/OpenFOAM/OpenFOAM-2.2.x
   /opt/OpenFOAM/OpenFOAM-2.2.x
 )
-
+message(STATUS ${OF22x_DIR})
 
 SET(OF22x_FOUND FALSE)
 
-IF(OF22x_BASHRC)
+IF(OF22x_DIR)
   set(OF22x_BASHRC "${OF22x_DIR}/etc/bashrc")
 
   execute_process(COMMAND ${CMAKE_SOURCE_DIR}/CMake/getOFCfgVar ${OF22x_BASHRC} print-c++FLAGS OUTPUT_VARIABLE OF22x_CXX_FLAGS)
   set(OF22x_CXX_FLAGS "${OF22x_CXX_FLAGS} -DOF22x")
-  message(STATUS "OF22x-CXXFLAGS=" ${OF22x_CXX_FLAGS})
 
   execute_process(COMMAND ${CMAKE_SOURCE_DIR}/CMake/getOFCfgVar ${OF22x_BASHRC} print-WM_OPTIONS OUTPUT_VARIABLE OF22x_WM_OPTIONS)
+  execute_process(COMMAND ${CMAKE_SOURCE_DIR}/CMake/getOFCfgVar ${OF22x_BASHRC} print-FOAM_EXT_LIBBIN OUTPUT_VARIABLE OF22x_FOAM_EXT_LIBBIN)
+  execute_process(COMMAND ${CMAKE_SOURCE_DIR}/CMake/getOFCfgVar ${OF22x_BASHRC} print-SCOTCH_ROOT OUTPUT_VARIABLE OF22x_SCOTCH_ROOT)
 
   set(OF22x_LIBSRC_DIR "${OF22x_DIR}/src")
-  message(STATUS "LIBSRC_DIR(OF22x)=" ${OF22x_LIBSRC_DIR})
   set(OF22x_LIB_DIR "${OF22x_DIR}/platforms/${OF22x_WM_OPTIONS}/lib")
-  message(STATUS "LIB_DIR(OF22x)=" ${OF22x_LIB_DIR})
   
   execute_process(COMMAND ${CMAKE_SOURCE_DIR}/CMake/getOFCfgVar ${OF22x_BASHRC} print-LINKLIBSO OUTPUT_VARIABLE OF22x_LINKLIBSO)
-  message(STATUS "LINKLIBSO(OF22x)=" ${OF22x_LINKLIBSO})
   execute_process(COMMAND ${CMAKE_SOURCE_DIR}/CMake/getOFCfgVar ${OF22x_BASHRC} print-LINKEXE OUTPUT_VARIABLE OF22x_LINKEXE)
-  message(STATUS "LINKEXE(OF22x)=" ${OF22x_LINKEXE})
   execute_process(COMMAND ${CMAKE_SOURCE_DIR}/CMake/getOFCfgVar ${OF22x_BASHRC} print-FOAM_MPI OUTPUT_VARIABLE OF22x_MPI)
-  message(STATUS "MPI(OF22x)=" ${OF22x_MPI})
-  
+´  
   macro (setup_target_OF22x targetname exename)
     set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${OF22x_CXX_FLAGS}")
     set(LINK_FLAGS "${LINK_FLAGS} ${OF22x_LINKEXE}")
-    link_directories(${OF22x_LIB_DIR} ${OF22x_LIB_DIR}/${OF22x_MPI})
+    link_directories(${OF22x_LIB_DIR} ${OF22x_LIB_DIR}/${OF22x_MPI} ${OF22x_FOAM_EXT_LIBBIN} "${OF22x_SCOTCH_ROOT}/lib")
     add_executable(${targetname} ${${targetname}_SOURCES})
     set_target_properties(${targetname} PROPERTIES OUTPUT_NAME ${exename})
     set_target_properties(${targetname} PROPERTIES RUNTIME_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/bin/OpenFOAM-2.2.x)
@@ -49,5 +43,5 @@ IF(OF22x_BASHRC)
   endmacro()
   
   SET(OF22x_FOUND TRUE)
-ENDIF(OF22x_BASHRC)
+ENDIF(OF22x_DIR)
 

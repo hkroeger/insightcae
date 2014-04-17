13b97b2a46351169e79f09fc714fe29a56597b83
Hannes Kroeger <hannes@kroegeronline.net>
2014-04-17 16:05:37 +0200

    limited SN-Grad scheme with local limiting coefficient

---
+++ src/extensions/openfoam/localLimitedSnGrad/localLimitedSnGrad.C
@@ -0,0 +1,100 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     |
+    \\  /    A nd           | Copyright (C) 2011-2012 OpenFOAM Foundation
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+\*---------------------------------------------------------------------------*/
+
+#include "fv.H"
+#include "localLimitedSnGrad.H"
+#include "volFields.H"
+#include "surfaceFields.H"
+#include "localMax.H"
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+namespace Foam
+{
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+namespace fv
+{
+
+// * * * * * * * * * * * * * * * * Destructor  * * * * * * * * * * * * * * * //
+
+template<class Type>
+localLimitedSnGrad<Type>::~localLimitedSnGrad()
+{}
+
+
+// * * * * * * * * * * * * * * * Member Functions  * * * * * * * * * * * * * //
+
+template<class Type>
+tmp<GeometricField<Type, fvsPatchField, surfaceMesh> >
+localLimitedSnGrad<Type>::correction
+(
+    const GeometricField<Type, fvPatchField, volMesh>& vf
+) const
+{
+    const GeometricField<Type, fvsPatchField, surfaceMesh> corr
+    (
+        correctedScheme_().correction(vf)
+    );
+    
+    const surfaceScalarField& limitCoeff = 
+      this->mesh().objectRegistry::template 
+      lookupObject<const surfaceScalarField>(limitCoeffFieldName_);
+
+    const surfaceScalarField limiter
+    (
+        min
+        (
+            limitCoeff*limitCoeffMult_
+           *mag(snGradScheme<Type>::snGrad(vf, deltaCoeffs(vf), "SndGrad"))
+           /(
+                (1.0 - limitCoeff*limitCoeffMult_)*mag(corr)
+              + dimensionedScalar("small", corr.dimensions(), SMALL)
+            ),
+            dimensionedScalar("one", dimless, 1.0)
+        )
+    );
+
+    if (fv::debug)
+    {
+        Info<< "localLimitedSnGrad :: limiter min: " << min(limiter.internalField())
+            << " max: "<< max(limiter.internalField())
+            << " avg: " << average(limiter.internalField()) << endl;
+    }
+
+    return limiter*corr;
+}
+
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+} // End namespace fv
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+} // End namespace Foam
+
+// ************************************************************************* //

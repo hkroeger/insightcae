73d4be0ba8e48ad6659975785d3cd88ffc93d56e
Hannes Kroeger <hannes@kroegeronline.net>
2014-09-08 16:13:11 +0200

    some fixes

--- src/toolkit/openfoam/snappyhexmesh.cpp
+++ src/toolkit/openfoam/snappyhexmesh.cpp
@@ -300,14 +300,14 @@ void snappyHexMesh(const OpenFOAMCase& ofc,
 
   
   // Check fraction of extruded faces on wall patches
-  boost::regex re_extrudedfaces("Extruding (.+) out of (.+) faces");
+  boost::regex re_extrudedfaces("^Extruding ([0-9]+) out of ([0-9]+) faces.*");
   boost::match_results<std::string::const_iterator> what;
   int exfaces=-1, totalfaces=-1;
   BOOST_FOREACH(const std::string& line, output)
   {
     if (boost::regex_match(line, what, re_extrudedfaces))
     {
-      //cout<< what[1]<<endl;
+      //cout<< "\""<<line<<"\""<<what[1]<<", "<<what[2]<<endl;
       exfaces=lexical_cast<int>(what[1]);
       totalfaces=lexical_cast<int>(what[2]);
     }
@@ -317,7 +317,10 @@ void snappyHexMesh(const OpenFOAMCase& ofc,
     double exfrac=double(exfaces)/double(totalfaces);
     if (exfrac<0.9)
     {
-      std::string msg="Prism layer covering is only "+str(format("%g")%(100.*exfrac))+"\% (<90%)!";
+      std::string msg=
+      "Prism layer covering is only "+str(format("%g")%(100.*exfrac))+"\% (<90%)!\n"
+      "Please reconsider prism layer thickness and tune number of prism layers!";
+      
       if (p.stopOnBadPrismLayer())
       {
 	throw insight::Exception(msg);

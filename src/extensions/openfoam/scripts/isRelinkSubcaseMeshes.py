0e0a9485a0e23a72d1445516e506ef91fc0c9a50
Hannes Kroeger <hannes@kroegeronline.net>
2014-12-03 12:55:29 +0100

    should also work if there are no files present yet

--- src/extensions/openfoam/scripts/isRelinkSubcaseMeshes.py
+++ src/extensions/openfoam/scripts/isRelinkSubcaseMeshes.py
@@ -4,8 +4,9 @@ for i in $*; do
  ( 
   cd $i/constant/polyMesh;
   for j in boundary faces owner neighbour points; do
-   if [ -h $j ]; then ln -svf ../../../constant/polyMesh/$j .; fi
-   if [ -h $j.gz ]; then ln -svf ../../../constant/polyMesh/$j.gz .; fi
+   O=../../../constant/polyMesh/$j
+   if [ -e $O.gz ]; then ln -svf $O.gz .; fi
+   if [ -e $O ]; then ln -svf $O .; fi
   done
  )
 done

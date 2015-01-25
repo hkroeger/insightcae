e9d651f54a7d1c474e92aa4b343c042239fc518e
Hannes Kroeger <hannes@kroegeronline.net>
2015-01-25 23:41:10 +0100

    update convertpressure tool

--- src/extensions/openfoam/convertPressure/convertPressure.C
+++ src/extensions/openfoam/convertPressure/convertPressure.C
@@ -30,16 +30,14 @@ using namespace Foam;
 
 int main(int argc, char *argv[])
 {
-    timeSelector::addOptions();
+//     timeSelector::addOptions();
 
+    argList::validArgs.append("from_name");
+    argList::validArgs.append("from_dim");
+    argList::validArgs.append("to_name");
     argList::validArgs.append("p0");
     argList::validArgs.append("rho");
     argList::validOptions.insert("pclip", "real pressure for clipping");
-    /*
-    argList::validOptions.insert("p0", "reference pressure");
-    argList::validOptions.insert("noShiftY", "");
-    argList::validOptions.insert("writeVTK", "");
-    */
 
 #   include "setRootCase.H"
 #   include "createTime.H"
@@ -47,60 +45,82 @@ int main(int argc, char *argv[])
     instantList timeDirs = timeSelector::select0(runTime, args);
     
 #   include "createMesh.H"
+    
+    word from_name(args.additionalArgs()[0]);
+    word to_name(args.additionalArgs()[2]);
+    
+    dimensionSet from_dim(IStringStream(args.additionalArgs()[1])());
 
     dimensionedScalar p0("p0", dimPressure, 1e5);
-    p0.value()=readScalar(IStringStream(args.additionalArgs()[0])());
+    p0.value()=readScalar(IStringStream(args.additionalArgs()[3])());
     
     dimensionedScalar rho("rho", dimDensity, 1);
-    rho.value()=readScalar(IStringStream(args.additionalArgs()[1])());
+    rho.value()=readScalar(IStringStream(args.additionalArgs()[4])());
     
     dimensionedScalar pclip("pclip", dimPressure, 0.0);
     if (args.optionFound("pclip"))
       pclip.value()=readScalar(IStringStream(args.options()["pclip"])());
     
-    forAll(timeDirs, timeI)
+    Info<< "Time = " << runTime.timeName() << endl;
+
+    Info << "Reading field "<<from_name<<"\n" << endl;
+    autoPtr<volScalarField> from_p
+    (
+      new volScalarField
+      (
+	  IOobject
+	  (
+	      from_name,
+	      runTime.timeName(),
+	      mesh,
+	      IOobject::MUST_READ,
+	      IOobject::AUTO_WRITE
+	  ),
+	  mesh
+      )
+    );
+    
+    from_p->dimensions().reset(from_dim);
+    
+    autoPtr<volScalarField> to_p
+    (
+      new volScalarField
+      (
+	  IOobject
+	  (
+	      to_name,
+	      runTime.timeName(),
+	      mesh,
+	      IOobject::MUST_READ,
+	      IOobject::AUTO_WRITE
+	  ),
+	  mesh
+      )
+    );
+
+
+    if ( from_dim == dimensionSet(1, -1, -2, 0, 0) )
     {
-        runTime.setTime(timeDirs[timeI], timeI);
-        Info<< "Time = " << runTime.timeName() << endl;
-        mesh.readUpdate();
-
-        Info << "Reading field p\n" << endl;
-        volScalarField p
-        (
-            IOobject
-            (
-                "p",
-                runTime.timeName(),
-                mesh,
-                IOobject::MUST_READ,
-                IOobject::AUTO_WRITE
-            ),
-            mesh
-        );
-
-	if ( p.dimensions() == dimensionSet(0, 2, -2, 0, 0) )
-	{
-	  // need to be converted into normalized pressure
-	  Info<<"Converting values into normalized pressure values."<<endl;
-	  p/=rho.value();
-	  p-=p0/rho;
-	}
-	else if ( p.dimensions() == dimensionSet(1, -1, -2, 0, 0) )
-	{
-	  Info<<"Converting values into real pressure values."<<endl;
-	  // need to be converted into real pressure
-	  p*=rho.value();
-	  p+=p0;
-	  p=max(pclip, p);
-	}
-	else
-	  FatalErrorIn("main")
-	  << "Don't know how the handle these pressure dimensions: "<<p.dimensions()
-	  <<abort(FatalError);
-
-	Info<<"min / max / avg = "<< min(p).value() << " / " << max(p).value() << " / " << average(p).value() << endl;  
-	p.write();
+      // need to be converted into normalized pressure
+      Info<<"Converting values into normalized pressure values."<<endl;
+      to_p()=from_p()/rho;
+      to_p()-=p0/rho;
     }
+    else if ( from_dim == dimensionSet(0, 2, -2, 0, 0) )
+    {
+      Info<<"Converting values into real pressure values."<<endl;
+      // need to be converted into real pressure
+      to_p()=from_p()*rho;
+      to_p()+=p0;
+      to_p()=max(pclip, to_p());
+    }
+    else
+      FatalErrorIn("main")
+      << "Don't know how the handle these pressure dimensions: "<<from_dim
+      <<abort(FatalError);
+
+    Info<<"min / max / avg = "<< min(to_p()).value() << " / " << max(to_p()).value() << " / " << average(to_p()).value() << endl;  
+    to_p->write();
 
     Info<< "End\n" << endl;
 

e2935d94110e43d6f5cdfdfb0bcaa3ecd89a6045
Hannes Kroeger <hannes@kroegeronline.net>
2015-06-08 22:39:08 +0200

    reflect modification of linkPolyMesh

--- src/extensions/openfoam/scripts/isRelinkSubcaseMeshes.py
+++ src/extensions/openfoam/scripts/isRelinkSubcaseMeshes.py
@@ -3,7 +3,7 @@
 for i in $*; do
  ( 
   cd $i/constant/polyMesh;
-  for j in boundary faces owner neighbour points; do
+  for j in faces owner neighbour points; do
    O=../../../constant/polyMesh/$j
    if [ -e $O.gz ]; then ln -svf $O.gz .; fi
    if [ -e $O ]; then ln -svf $O .; fi

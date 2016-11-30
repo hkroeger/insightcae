940295517431ce26cb7573a4c6af83af457e5c59
Hannes Kroeger <hannes@kroegeronline.net>
2016-11-30 08:58:24 +0100

    OF unit tests

---
+++ src/test/openfoam/simpleFoam/runOFTest_kOmegaSST.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+OFID=$1
+BASHSCR=$2
+DATADIR=$3
+shift 3 # remove CMD line args, causes problems with some bashrcs
+
+source ${BASHSCR}
+if [ -d $OFID ]; then rm -rf $OFID; fi; mkdir $OFID && cd $OFID && (
+
+isofCaseBuilder -sb $DATADIR/simpleFoamCase.iscb &&\
+blockMesh && \
+isofCaseBuilder -b $DATADIR/simpleFoamCase.iscb $DATADIR/RASmodel_kOmegaSST.iscb &&\
+simpleFoam 
+
+)

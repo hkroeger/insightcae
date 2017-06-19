9702a4c989df161eea18284026d55d24646dea29
Hannes Kroeger <hannes@kroegeronline.net>
2017-06-19 09:31:46 +0200

    update OF version number for OF+

--- CMake/FindOpenFOAM-plus.cmake
+++ CMake/FindOpenFOAM-plus.cmake
@@ -211,7 +211,7 @@ randomProcesses
   set(OFplus_INSIGHT_BIN "${CMAKE_BINARY_DIR}/bin/OpenFOAM-${OFplus_WM_PROJECT_VERSION}")
   set(OFplus_INSIGHT_LIB "${CMAKE_BINARY_DIR}/lib/OpenFOAM-${OFplus_WM_PROJECT_VERSION}")
 
-  addOFConfig(OFplus ofplus 302)
+  addOFConfig(OFplus ofplus 400)
 #   list(APPEND INSIGHT_OFES_VARCONTENT "OF23x@`find \\\${PATH//:/ } -maxdepth 1 -name insight.bashrc.of23x -print -quit`#230")
 #   set(INSIGHT_OF_ALIASES "${INSIGHT_OF_ALIASES}
 # alias of23x=\"source insight.bashrc.of23x\"

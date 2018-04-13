3e779adefc2c235dea085bf8c510301ac8a446e2
Hannes Kroeger <hannes@kroegeronline.net>
2018-04-13 17:10:56 +0200

    offset feature

---
+++ src/cad/cadfeatures/offset.h
@@ -0,0 +1,58 @@
+/*
+ * This file is part of Insight CAE, a workbench for Computer-Aided Engineering
+ * Copyright (C) 2014  Hannes Kroeger <hannes@kroegeronline.net>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+#ifndef OFFSET_H
+#define OFFSET_H
+
+
+#include "cadfeature.h"
+
+namespace insight {
+namespace cad {
+
+
+
+
+class Offset
+    : public Feature
+{
+    FeaturePtr shell_;
+    ScalarPtr thickness_;
+    ScalarPtr tol_;
+
+    Offset ( FeaturePtr shell, ScalarPtr thickness, ScalarPtr tol=scalarconst ( Precision::Confusion() ) );
+
+    virtual size_t calcHash() const;
+    virtual void build();
+
+public:
+    declareType ( "Offset" );
+    Offset ();
+    static FeaturePtr create ( FeaturePtr shell, ScalarPtr thickness, ScalarPtr tol=scalarconst ( Precision::Confusion() ) );
+
+
+    virtual void insertrule ( parser::ISCADParser& ruleset ) const;
+    virtual FeatureCmdInfoList ruleDocumentation() const;
+};
+
+}
+}
+
+
+#endif // OFFSET_H

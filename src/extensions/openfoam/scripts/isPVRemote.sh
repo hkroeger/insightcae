5c43e2fbf4dcb98d21becf91a76b3a4d0b5d8a4f
Hannes Kroeger <hannes@kroegeronline.net>
2018-12-30 21:02:43 +0100

    remote paraview execution script

--- src/extensions/openfoam/scripts/isPVRemote.sh
+++ src/extensions/openfoam/scripts/isPVRemote.sh
@@ -1,16 +1,38 @@
 #!/bin/bash
 
-SERVER=titanp
+OPTIND=1         # Reset in case getopts has been used previously in the shell.
+SERVER=localhost
+
+if [ -e meta.ofc ]; then
+read SERVER DIR << EOF
+$(cat meta.ofc|tr ':' ' ')
+EOF
+fi
+
+while getopts "h?s" opt; do
+    case "$opt" in
+    h|\?)
+        echo "-s: server address"
+        exit 0
+        ;;
+    s)  SERVER=s
+        ;;
+    esac
+done
+
+#OBJ=${@:$OPTIND:1}
+#TOOL=${@:$OPTIND+1:1}
+#RES=${@:$OPTIND+2:1}
 
 read LABEL PORT << EOF
-$(ssh $SERVER /home/hk354/bin/isPVFindPort.sh|tail -n 1)
+$(ssh $SERVER isPVFindPort.sh|tail -n 1)
 EOF
 
 if [ "$LABEL" != "PORT" ]; then
  echo expected "PORT", got "$LABEL"
 fi
 
-ssh -L$PORT:localhost:$PORT titanp pvserver --server-port=$PORT &
+ssh -L$PORT:localhost:$PORT $SERVER "cd $DIR ; pvserver --server-port=$PORT" &
 sleep 1
 
-paraview --server-url=cs://localhost:$PORT
+paraview --server-url=cs://localhost:$PORT --data=$DIR/system/controlDict

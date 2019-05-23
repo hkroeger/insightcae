bf29bd3a2c4b81bf67a8845856b8191d3b863b12
Hannes Kroeger <hannes@kroegeronline.net>
2019-05-23 22:45:30 +0200

    add time step limit check

--- src/toolkit/openfoam/caseelements/basiccaseelements.cpp
+++ src/toolkit/openfoam/caseelements/basiccaseelements.cpp
@@ -101,6 +101,45 @@ bool gravity::isUnique() const
 }
 
 
+
+defineType(minimumTimestepLimit);
+addToOpenFOAMCaseElementFactoryTable(minimumTimestepLimit);
+
+minimumTimestepLimit::minimumTimestepLimit( OpenFOAMCase& c, const ParameterSet& ps )
+: OpenFOAMCaseElement(c, "minimumTimestepLimit"),
+  p_(ps)
+{
+}
+
+void minimumTimestepLimit::addIntoDictionaries(OFdicts& dictionaries) const
+{
+  OFDictData::dict Fd, cd, mindtd;
+
+  Fd["type"]="runTimeControl";
+  mindtd["type"]="minTimeStep";
+  mindtd["minValue"]=p_.minDT;
+  cd["mindt"]=mindtd;
+  Fd["conditions"]=cd;
+
+  OFDictData::list fol;
+  fol.push_back("\"libutilityFunctionObjects.so\"");
+  Fd["functionObjectLibs"]=fol;
+
+
+  OFDictData::dict& controlDict=dictionaries.lookupDict("system/controlDict");
+  controlDict.addSubDictIfNonexistent("functions")["minimumTimestepLimit"]=Fd;
+}
+
+ParameterSet minimumTimestepLimit::defaultParameters()
+{
+    return Parameters::makeDefault();
+}
+std::string minimumTimestepLimit::category()
+{
+  return "Body Force";
+}
+
+
 defineType(mirrorMesh);
 addToOpenFOAMCaseElementFactoryTable(mirrorMesh);
 
--- src/toolkit/openfoam/caseelements/basiccaseelements.h
+++ src/toolkit/openfoam/caseelements/basiccaseelements.h
@@ -93,6 +93,33 @@ public:
 
 
 
+class minimumTimestepLimit
+    : public OpenFOAMCaseElement
+{
+
+public:
+#include "basiccaseelements__minimumTimestepLimit__Parameters.h"
+/*
+PARAMETERSET>>> minimumTimestepLimit Parameters
+
+minDT = double 1e-10 "Minimum time step size. If automatic time step adaption underruns this value, output is written and simulation is stopped."
+
+<<<PARAMETERSET
+*/
+
+protected:
+    Parameters p_;
+
+public:
+    declareType ( "minimumTimestepLimit" );
+    minimumTimestepLimit ( OpenFOAMCase& c, const ParameterSet& ps = Parameters::makeDefault() );
+    virtual void addIntoDictionaries ( OFdicts& dictionaries ) const;
+
+    static ParameterSet defaultParameters();
+    static std::string category();
+};
+
+
 class mirrorMesh
     : public OpenFOAMCaseElement
 {

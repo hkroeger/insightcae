f0875d0dd2bc3c62077e8901a6ab569d4873d31e
Hannes Kroeger <hannes@kroegeronline.net>
2019-09-14 13:37:23 +0200

    fix syntax error in script generation

---
+++ src/toolkit/openfoam/caseelements/dynamicmeshcaseelements.cpp
@@ -0,0 +1,6 @@
+#include "dynamicmeshcaseelements.h"
+
+dynamicmeshcaseelements::dynamicmeshcaseelements()
+{
+
+}
---
+++ src/toolkit/openfoam/caseelements/dynamicmeshcaseelements.h
@@ -0,0 +1,11 @@
+#ifndef DYNAMICMESHCASEELEMENTS_H
+#define DYNAMICMESHCASEELEMENTS_H
+
+
+class dynamicmeshcaseelements
+{
+public:
+  dynamicmeshcaseelements();
+};
+
+#endif // DYNAMICMESHCASEELEMENTS_H

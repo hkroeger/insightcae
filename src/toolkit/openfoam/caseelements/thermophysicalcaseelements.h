c3a4ff8d2938e7b6c86ad4e22fbedcd4440126ae
Hannes Kroeger <hannes@kroegeronline.net>
2020-06-28 23:45:59 +0200

    add species library

--- src/toolkit/openfoam/caseelements/thermophysicalcaseelements.h
+++ src/toolkit/openfoam/caseelements/thermophysicalcaseelements.h
@@ -23,6 +23,11 @@
 
 #include "openfoam/caseelements/basic/thermodynamicmodel.h"
 
+#include <vector>
+#include <string>
+#include <array>
+#include <map>
+
 namespace insight {
 
 
@@ -59,6 +64,7 @@ class perfectGasSinglePhaseThermophysicalProperties
     : public thermodynamicModel
 {
 
+
 public:
 #include "thermophysicalcaseelements__perfectGasSinglePhaseThermophysicalProperties__Parameters.h"
 /*
@@ -90,6 +96,123 @@ public:
 
 
 double sutherland_As(double mu, double Ts);
+
+
+class SpeciesData
+{
+
+  static void modifyDefaults(ParameterSet& ps);
+
+public:
+
+  struct ElementData {
+    int id;
+    std::string name;
+    double M;
+  };
+
+  // from https://www.qmul.ac.uk/sbcs/iupac/AtWt/
+  static const std::map<std::string, SpeciesData::ElementData> elements;
+
+
+#include "thermophysicalcaseelements__SpeciesData__Parameters.h"
+
+/*
+PARAMETERSET>>> SpeciesData Parameters
+addTo_makeDefault { modifyDefaults(p); }
+
+name = string "N2" "Name of specie (i.e. the field name). Has to be unique!" *necessary
+
+properties = selectablesubset {{
+
+  fromLibrary set {
+   specie = selection ( dummy ) dummy "Species library entry"
+  }
+
+  custom set {
+
+    thermo = selectablesubset {{
+
+     constant
+     set {
+       Cp = double 1007 "[J/kg/K] Heat capacity"
+       Hf = double 0. "[J/kg] Enthalpy of formation"
+     }
+
+     janaf
+     set {
+      Tlow = double 100 "[T] Lower temperature bound of approximation"
+      Thi = double 6000 "[T] Upper temperature bound of approximation"
+      Tmid = double 1000 "[T] Switching temperature between hi and low polynomial"
+
+      coeffs_lo = vector (3.5309628 -0.0001236595 -5.0299339e-07 2.4352768e-09 -1.4087954e-12 -1046.9637 2.9674391) "Lower temperature polynomial coefficients"
+      coeffs_hi = vector (2.9525407 0.0013968838 -4.9262577e-07 7.8600091e-11 -4.6074978e-15 -923.93753 5.8718221) "Higher temperature polynomial coefficients"
+     }
+
+    }} constant "Thermodynamics properties"
+
+
+
+
+    transport = selectablesubset {{
+
+     constant
+     set {
+       mu = double 1.8e-5 "Dynamic viscosity"
+       Pr = double 0.7 "Prandtl number"
+     }
+
+     sutherland
+     set {
+       mu = double 9.41e-6 "Dynamic viscosity at $T_{ref}$"
+       Tref = double 440 "Reference temperature $T_{ref}$"
+       Pr = double 0.7 "Prandtl number"
+     }
+
+    }} constant "Transport properties"
+
+
+
+    elements = array [ set {
+     element = string "N" "Name of the element"
+     number = int 2 "Number of atoms per molecule"
+     } ] *1 "Elemental composition"
+
+   }
+
+}} fromLibrary "Data of the specie."
+
+<<<PARAMETERSET
+*/
+
+protected:
+//  Parameters p_;
+  std::string name_;
+  Parameters::properties_custom_type p_;
+
+  static struct SpeciesLibrary
+      : public std::map<std::string, Parameters::properties_custom_type>
+  {
+    declareFactoryTable(SpeciesLibrary, LIST(const ParameterSet& ps = ParameterSet() ), LIST(ps));
+
+    SpeciesLibrary();
+  } speciesLibrary_;
+
+
+public:
+  SpeciesData(const ParameterSet& ps);
+
+  double M() const;
+
+  std::string transportType() const;
+  std::string thermoType() const;
+
+  void insertSpecieEntries(OFDictData::dict& d) const;
+  void insertThermodynamicsEntries(OFDictData::dict& d) const;
+  void insertTransportEntries(OFDictData::dict& d) const;
+  void insertElementsEntries(OFDictData::dict& d) const;
+};
+
 
 class compressibleSinglePhaseThermophysicalProperties
     : public thermodynamicModel
@@ -183,31 +306,66 @@ public:
 
 
 
-class multispeciesThermodynamics
-{
-public:
-  typedef std::map<std::string, double> SpeciesList;
-  
-protected:
-  SpeciesList defaultComposition_;
 
-public:
-  inline const SpeciesList& defaultComposition() const { return defaultComposition_; }
-};
 
 class detailedGasReactionThermodynamics
-: public thermodynamicModel,
-  public multispeciesThermodynamics
+: public thermodynamicModel
 {
+
+public:
+  typedef std::map<std::string, SpeciesData> SpeciesList;
+  typedef std::map<std::string, double> SpeciesMassFractionList;
+
+  std::vector<std::string> speciesNames() const;
+  SpeciesMassFractionList defaultComposition() const;
+
+protected:
+  std::string getTransportType() const;
+  std::string getThermoType() const;
+
 public:
 #include "thermophysicalcaseelements__detailedGasReactionThermodynamics__Parameters.h"
 /*
 PARAMETERSET>>> detailedGasReactionThermodynamics Parameters
 
 inertSpecie = string "N2" ""
-foamChemistryFile = path "" ""
-foamChemistryThermoFile = path "" ""
-Cmix = double 1.0 ""
+
+composition = selectablesubset {{
+
+  fromFile set {
+    foamChemistryThermoFile = path "" "" *necessary
+  }
+
+  fromList set {
+    species = array [
+     includedset "insight::SpeciesData::Parameters"
+    ] *1 "Species in the mixture"
+  }
+
+ }} fromList ""
+
+
+
+reactions = selectablesubset {{
+
+ none set {
+ }
+
+ fromFile set {
+    foamChemistryFile = path "" "" *necessary
+ }
+
+}} none ""
+
+
+
+combustionModel = selectablesubset {{
+ none set { }
+ EDC set { }
+ PaSR set {
+  Cmix = double 1.0 ""
+ }
+}} EDC "The combustion model"
 
 <<<PARAMETERSET
 */

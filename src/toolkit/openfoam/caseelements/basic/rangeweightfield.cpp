c6fe99c64e5d1fa41eb1765c0ab9179838b555dc
Hannes Kroeger <hannes@kroegeronline.net>
2020-07-29 17:59:05 +0200

    add interface to new FO

--- src/toolkit/openfoam/caseelements/basic/rangeweightfield.cpp
+++ src/toolkit/openfoam/caseelements/basic/rangeweightfield.cpp
@@ -1,6 +1,50 @@
 #include "rangeweightfield.h"
 
-rangeWeightField::rangeWeightField()
-{
 
+#include "openfoam/ofdicts.h"
+#include "openfoam/openfoamdict.h"
+
+
+namespace insight {
+
+defineType(rangeWeightField);
+addToOpenFOAMCaseElementFactoryTable(rangeWeightField);
+
+rangeWeightField::rangeWeightField( OpenFOAMCase& c, const ParameterSet& ps )
+: OpenFOAMCaseElement(c, "rangeWeightField", ps),
+  p_(ps)
+{
 }
+
+void rangeWeightField::addIntoDictionaries(OFdicts& dictionaries) const
+{
+  OFDictData::dict& cd=dictionaries.lookupDict("system/controlDict");
+  cd.getList("libs").insertNoDuplicate("\"librangeWeightField.so\"");
+
+  OFDictData::dict fod;
+  fod["type"]="rangeWeightField";
+
+  fod["sourceFieldName"]=p_.sourceFieldName;
+  fod["outputFieldName"]=p_.outputFieldName;
+  fod["min"]=p_.min;
+  fod["max"]=p_.max;
+
+  if (!p_.multiplyFieldName.empty())
+    fod["multiplyFieldName"]=p_.multiplyFieldName;
+
+  cd.subDict("functions")[p_.outputFieldName+"_functionObject"] = fod;
+}
+
+
+std::string rangeWeightField::category()
+{
+  return "PostProcessing";
+}
+
+bool rangeWeightField::isUnique() const
+{
+  return true;
+}
+
+} // namespace insight
+
--- src/toolkit/openfoam/caseelements/basic/rangeweightfield.h
+++ src/toolkit/openfoam/caseelements/basic/rangeweightfield.h
@@ -1,11 +1,41 @@
 #ifndef RANGEWEIGHTFIELD_H
 #define RANGEWEIGHTFIELD_H
 
+#include "openfoam/caseelements/openfoamcaseelement.h"
+
+namespace insight {
 
 class rangeWeightField
+    : public OpenFOAMCaseElement
 {
+
 public:
-  rangeWeightField();
+#include "rangeweightfield__rangeWeightField__Parameters.h"
+/*
+PARAMETERSET>>> rangeWeightField Parameters
+
+sourceFieldName = string "" "Source field name"
+min = double 0 "minimum value"
+max = double 1 "maximum value"
+outputFieldName = string "" "Output field name"
+multiplyFieldName = string "" "Optional name of field with which the output field is to be multiplied. No multiplication is done, if the field is empty."
+
+<<<PARAMETERSET
+*/
+
+protected:
+    Parameters p_;
+
+public:
+    declareType ( "rangeWeightField" );
+    rangeWeightField ( OpenFOAMCase& c, const ParameterSet& ps = Parameters::makeDefault() );
+    void addIntoDictionaries ( OFdicts& dictionaries ) const override;
+    virtual bool isUnique() const;
+
+    static std::string category();
 };
+
+} // namespace insight
+
 
 #endif // RANGEWEIGHTFIELD_H

53298370eeb28df512c6fcd198377787606fdab0
Hannes Kroeger <hannes@kroegeronline.net>
2021-01-29 16:51:51 +0100

    reorganize CA export file handling

--- src/toolkit/code_aster/caexportfile.cpp
+++ src/toolkit/code_aster/caexportfile.cpp
@@ -31,6 +31,9 @@ namespace fs=boost::filesystem;
 namespace insight
 {
 
+
+
+
 CAExportFile::CAExportFile(const boost::filesystem::path& commFile, std::string version, int t_max, int mem_max)
   : boost::filesystem::path(commFile.parent_path()/(commFile.filename().stem().string()+".export")),
     t_max_(t_max),
@@ -42,14 +45,97 @@ CAExportFile::CAExportFile(const boost::filesystem::path& commFile, std::string 
   setRMedFile(commFile.parent_path()/(commFile.filename().stem().string()+".rmed"));
 }
 
+
+
+
 CAExportFile::~CAExportFile()
 {
 }
+
+
+
+
+void CAExportFile::setNP(int np_omp, int np_mpi)
+{
+  np_omp_=np_omp;
+  np_mpi_=np_mpi;
+}
+
+
+
+
+void CAExportFile::setMessFile(const boost::filesystem::path& fn)
+{
+  //messFile_.reset(new boost::filesystem::path(fn));
+  outputFiles_[6] = FileInfo{MESS, fn};
+}
+
+
+
+
+void CAExportFile::setRMedFile(const boost::filesystem::path& fn)
+{
+  //rmedFile_.reset(new boost::filesystem::path(fn));
+  outputFiles_[80] = FileInfo{RMED, fn};
+}
+
+
+
+
+boost::filesystem::path CAExportFile::RMedFile() const
+{
+  auto of = outputFiles_.find(80);
+  if (of==outputFiles_.end())
+    return boost::filesystem::path();
+  else
+    return of->second.filePath;
+}
+
+
+
+
+void CAExportFile::addMeshMedFile(const boost::filesystem::path& fn, int unit)
+{
+  if (unit<0)
+  {
+    std::set<int> mmedUnits;
+    for (const auto& of: inputFiles_)
+      if (of.second.fileType==MMED)
+        mmedUnits.insert(of.first);
+
+    if (mmedUnits.size()==0)
+    {
+      unit = 20;
+    }
+    else
+    {
+      unit = *mmedUnits.rbegin() +1;
+    }
+  }
+  inputFiles_[unit] = FileInfo{MMED, fn};
+}
+
+std::set<boost::filesystem::path> CAExportFile::outputFiles(const std::string &fileType) const
+{
+  std::set<boost::filesystem::path> result;
+
+  for (const auto& of: outputFiles_)
+    if (of.second.fileType==fileType)
+      result.insert(of.second.filePath);
+
+  return result;
+}
+
+
+
 
 const boost::filesystem::path &CAExportFile::commFilePath() const
 {
   return commFile_;
 }
+
+
+
 
 void CAExportFile::writeFile(const fs::path& exportFileName) const
 {
@@ -84,21 +170,16 @@ void CAExportFile::writeFile(const fs::path& exportFileName) const
   <<"A tpmax " << t_max_ << "\n"
   <<"F comm " << commFile_.c_str() << " D  1\n";
   
-  for (const FileList::value_type& item: mmedFiles_)
+  for (const FileList::value_type& item: inputFiles_)
   {
-    f << "F mmed " << item.second.string() << " D  " << item.first << "\n";
+    f << "F " << item.second.fileType << " " << item.second.filePath.string() << " D  " << item.first << "\n";
   }
-  
-  if (rmedFile_.get())
+
+  for (const FileList::value_type& item: outputFiles_)
   {
-    f << "F rmed " << rmedFile_->string() << " R  80\n";
+    f << "F " << item.second.fileType << " " << item.second.filePath.string() << " R  " << item.first << "\n";
   }
-  
-  if (messFile_.get())
-  {
-    f << "F mess " << messFile_->string() << " R  6\n";
-  }
-  
+
 }
 
 
--- src/toolkit/code_aster/caexportfile.h
+++ src/toolkit/code_aster/caexportfile.h
@@ -32,60 +32,43 @@ class CAExportFile
 {
   
 public:
-  typedef std::map<int, boost::filesystem::path> FileList;
+  std::string
+    MESS="mess",
+    RMED="rmed",
+    MMED="mmed"
+  ;
+
+  struct FileInfo
+  {
+    std::string fileType;
+    boost::filesystem::path filePath;
+  };
+  typedef std::map<int, FileInfo> FileList;
   
 protected:
   int t_max_, mem_max_;
   std::string version_;
   
   boost::filesystem::path commFile_;
-  FileList mmedFiles_;
+  FileList inputFiles_, outputFiles_;
   
-  std::unique_ptr<boost::filesystem::path> messFile_;
-  std::unique_ptr<boost::filesystem::path> rmedFile_;
-
   int np_omp_, np_mpi_;
   
 public:
     CAExportFile(const boost::filesystem::path& commFile, std::string version="stable", int t_max=7*24*60*60, int mem_max=0 /*MB*/);
     ~CAExportFile();
 
-    inline void setNP(int np_omp=1, int np_mpi=1)
-    {
-      np_omp_=np_omp;
-      np_mpi_=np_mpi;
-    }
-    
-    inline void setMessFile(const boost::filesystem::path& fn)
-    {
-      messFile_.reset(new boost::filesystem::path(fn));
-    }
-    
-    inline void setRMedFile(const boost::filesystem::path& fn)
-    {
-      rmedFile_.reset(new boost::filesystem::path(fn));
-    }
+    void setNP(int np_omp=1, int np_mpi=1);
 
-    inline boost::filesystem::path RMedFile() const
-    {
-      return *rmedFile_;
-    }
-    
-    inline void addMeshMedFile(const boost::filesystem::path& fn, int unit=-1)
-    {
-      if (unit<0)
-      {
-	if (mmedFiles_.size()==0) 
-	  unit=20;
-	else
-	{
-	  unit=mmedFiles_.rbegin()->first +1;
-	}
-      }
-      mmedFiles_[unit]=fn;
-    }
+    void setMessFile(const boost::filesystem::path& fn);
+    void setRMedFile(const boost::filesystem::path& fn);
+    void addMeshMedFile(const boost::filesystem::path& fn, int unit=-1);
+
+    std::set<boost::filesystem::path> outputFiles(const std::string& fileType="mmed") const;
 
     const boost::filesystem::path& commFilePath() const;
+
+    boost::filesystem::path RMedFile() const;
 
     virtual void writeFile(const boost::filesystem::path& exportFileName="") const;
 };

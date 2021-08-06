899547acc0e32afdc384a1ad102748699db655a2
Hannes Kroeger <hannes@kroegeronline.net>
2021-08-06 14:21:16 +0200

    windows installer generation scripts

--- generateInsightCAEWindowsMSI.py
+++ generateInsightCAEWindowsMSI.py
@@ -1,109 +1,41 @@
 #!/usr/bin/env python3
 # This Python file uses the following encoding: utf-8
 
-import os,sys,subprocess,re
+import os,sys,subprocess,re,copy
 import xml.etree.cElementTree as ET
+import importlib.util
+sourcePath = os.path.dirname(os.path.realpath(__file__))
+spec = importlib.util.spec_from_file_location("module.name", os.path.join(sourcePath, "wix.py"))
+wix = importlib.util.module_from_spec(spec)
+spec.loader.exec_module(wix)
+
+from optparse import OptionParser
+
+parser = OptionParser()
+
+parser.add_option("-c", "--customer", dest="customer", metavar='customer', default="ce",
+                  help="label of customer")
+
+parser.add_option("-x", "--mxepath", dest="mxepath", metavar='mxepath', default="/home/hannes/Programme/mxe",
+                help="path to mxe cross compile environment")
+
+(opts, args) = parser.parse_args()
+
 
 packageName="InsightCAE"
+
+if not (opts.customer=="ce" or opts.customer=="ce-dev"):
+    packageName+=" ("+opts.customer+")"
+
 versionMajor=3
 versionMinor=0
 versionPatch=0
 fullVersion=(versionMajor,versionMinor,versionPatch)
 
-def sanitizeId(orgname):
-    clean="id"+"".join(filter(str.isalnum, orgname))
-    print (orgname, " ==>> ", clean)
-    return clean
 
-def toWinePath(linuxPath):
-    return "z:"+os.path.abspath(linuxPath).replace("/", "\\")
+packageFileName=copy.copy(packageName)
+packageFileName.replace(" ", "_")
 
-
-class WixInput:
-    wix = ET.Element("Wix", attrib={'xmlns': 'http://schemas.microsoft.com/wix/2006/wi'})
-
-    Product = ET.SubElement(wix, "Product", attrib={
-    'Name':packageName,
-    'Manufacturer': 'silentdynamics GmbH',
-    'Id': 'd6e296e3-61c2-44b4-8736-be4568091ff4',
-    'UpgradeCode': '87be4b31-c7c0-493c-ac97-3736a3417867',
-    'Language': '1033',
-    'Codepage': '1252',
-    'Version': '%d.%d.%d'%fullVersion
-    })
-
-    Package=ET.SubElement(Product, "Package", attrib={
-    'Id': '*',
-    'Keywords': 'Installer',
-    'Description': "silentdynamics InsightCAE for Windows Installer",
-    'Comments': 'comment',
-    'Manufacturer': 'silentdynamics GmbH',
-    'InstallerVersion': '100',
-    'Languages': '1033',
-    'Compressed': 'yes',
-    'SummaryCodepage': '1252'
-    })
-
-    Media=ET.SubElement(Product, "Media", attrib={
-    'Id': '1',
-    'Cabinet': packageName+'.cab',
-    'EmbedCab': 'yes',
-    'DiskPrompt': 'Disk #1'
-    })
-
-    Property=ET.SubElement(Product, "Property", attrib={
-    'Id': 'DiskPrompt',
-    'Value': "silentdynamics InsightCAE %d.%d.%d Installation [1]"%fullVersion
-    })
-
-    INSTALLDIR=ET.SubElement(
-        ET.SubElement( ET.SubElement( ET.SubElement(Product,
-                "Directory", attrib={'Id':'TARGETDIR', 'Name':'SourceDir'}),
-            "Directory", attrib={'Id':'ProgramFilesFolder', 'Name':'PFiles'}),
-          "Directory", attrib={'Id':'silentdynamics', 'Name':'silentdynamics'}),
-         "Directory", attrib={'Id':'INSTALLDIR', 'Name':packageName+'-%d.%d.%d'%fullVersion})
-
-
-    Feature=ET.SubElement(Product, "Feature", attrib={
-    'Id': 'Complete',
-    'Level': '1'
-    })
-
-    subdirs={}
-
-    def getDirNode(self, relpath):
-        if relpath=="" or relpath==".":
-            return self.INSTALLDIR
-        elif not relpath in self.subdirs:
-            #create
-            pd=self.getDirNode(os.path.dirname(relpath))
-            bn=os.path.basename(relpath)
-            node=ET.SubElement(pd, "Directory", attrib={'Id':sanitizeId(bn), 'Name':bn})
-            self.subdirs[relpath]=node
-        return self.subdirs[relpath]
-
-
-    def addFiles(self, fileList):
-        for bn,(compn,sourcef,targdir) in fileList.items():
-            node=self.getDirNode(targdir)
-            Component=ET.SubElement(node, "Component", attrib={ 'Id': compn, 'Guid': '*' })
-            File=ET.SubElement(Component, "File", attrib={ 'Id': compn, 'Name': bn, 'DiskId': '1', 'Source':toWinePath(sourcef), 'KeyPath': 'yes' })
-
-            ET.SubElement(self.Feature, "ComponentRef", attrib={'Id': compn})
-
-    def addEnvVar(self,name,value,guid):
-        varcomp=ET.SubElement(wxs.INSTALLDIR, "Component", attrib={'Id':name, 'Guid':guid})
-        var=ET.SubElement(varcomp, "Environment", attrib={'Id':name, 'Name':name,
-                'Action': 'set', 'Permanent': 'yes', 'System':'yes', 'Part': 'all', 'Value':value})
-        ET.SubElement(self.Feature, "ComponentRef", attrib={'Id':name})
-
-    def write(self, fn):
-        tree = ET.ElementTree(self.wix)
-        tree.write(fn)
-
-
-
-mxepath="/home/hannes/Programme/mxe"
 
 directories=["bin", "share/insight"]
 
@@ -113,32 +45,27 @@ if not (os.path.exists("./bin") and os.path.exists("./share/insight")):
 
 
 
-
-
-
 def getDependencies(file):
     deps=set()
     ext=os.path.splitext(file)[-1]
     if ext==".exe" or ext==".dll":
-        ret=subprocess.run([os.path.join(mxepath,"usr/x86_64-pc-linux-gnu/bin/peldd"), file], stdout=subprocess.PIPE)
+        ret=subprocess.run([os.path.join(opts.mxepath,"usr/x86_64-pc-linux-gnu/bin/peldd"), file], stdout=subprocess.PIPE)
         deps=set([l.decode('UTF-8') for l in ret.stdout.split()])
         print(file, " : ", deps)
     return deps
 
 def findFile(f):
-    ret=subprocess.run(["find", os.path.join(mxepath, "usr/i686-w64-mingw32.shared"), "-iname", f], stdout=subprocess.PIPE)
+    ret=subprocess.run(["find", os.path.join(opts.mxepath, "usr/i686-w64-mingw32.shared"), "-iname", f], stdout=subprocess.PIPE)
+    #ret=subprocess.run(["locate", "-i", "-r", "^"+os.path.join(opts.mxepath, "usr/i686-w64-mingw32.shared")+".*"+f+"$"], stdout=subprocess.PIPE)
     found=[l.decode('UTF-8') for l in ret.stdout.split()]
     print("found:", found)
     if len(found)>1:
-        raise RuntimeError("multiple candidates for "+f+" found! "+str(found))
+        raise RuntimeError("multiple candidaProgramMenuFoldertes for "+f+" found! "+str(found))
     if len(found)==0:
         print("No condidate for",f, "found.")
         return None
 
     return found[-1]
-
-
-
 
 
 class FileList:
@@ -148,7 +75,7 @@ class FileList:
     def addFile(self, sourceFile, targetDir):
         bn=os.path.basename(sourceFile)
         print("add file: ", sourceFile, targetDir)
-        self.files[bn] = ( sanitizeId(bn), sourceFile, targetDir )
+        self.files[bn] = ( wix.sanitizeId(bn), sourceFile, targetDir )
 
     def addFiles(self, subdir, pattern=".*", targetprefix="", removeprefix=""):
         files=filter(
@@ -182,24 +109,30 @@ class FileList:
                     fdf=findFile(df)
                     if not fdf is None:
                         added+=1
-                        self.files[df]=( sanitizeId(df), fdf, "bin" )
+                        self.files[df]=( wix.sanitizeId(df), fdf, "bin" )
 
         print("added", added, "dependencies")
         return added
 
 
 
-wxs=WixInput()
+wxs=wix.WixInput(
+    packageName,
+    'd6e296e3-61c2-44b4-8736-be4568091ff4', '87be4b31-c7c0-493c-ac97-3736a3417867',
+    fullVersion,
+    menuentry=("InsightCAE", '5f61d82b-1c22-4e54-b9b6-b7cad11e7aee') )
+
 fl=FileList()
 
 for d in directories:
     fl.addFiles(d)
 
-fl.addFile( os.path.join(mxepath, "usr/i686-w64-mingw32.shared/qt5/plugins/platforms/qwindows.dll"), "bin/plugins/platforms" )
+fl.addFile( os.path.join(opts.mxepath, "usr/i686-w64-mingw32.shared/qt5/plugins/platforms/qwindows.dll"), "bin/plugins/platforms" )
 
-sp=os.path.join(mxepath, "usr/i686-w64-mingw32.shared/share/oce-0.18/src/Shaders")
+sp=os.path.join(opts.mxepath, "usr/i686-w64-mingw32.shared/share/oce-0.18/src/Shaders")
 fl.addFiles( sp, ".*", "share/oce-0.18/Shaders", sp)
 wxs.addEnvVar('CSF_ShadersDirectory','[INSTALLDIR]share\\oce-0.18\\Shaders','3b4ab883-6637-4969-a006-d47ab6e8072b')
+wxs.addEnvVar('INSIGHT_GLOBALSHAREDDIRS','[INSTALLDIR]share\\insight','2dba120f-def7-441d-93a8-abef0b5448e0')
 
 while (fl.addDependencies()>0):
     pass
@@ -208,6 +141,9 @@ print(fl.files)
 
 wxs.addFiles(fl.files)
 
-scriptPath = os.path.dirname(os.path.realpath(__file__))
-wxs.write(packageName+".wxs")
-subprocess.run([os.path.join(scriptPath,"wixbuild.sh"), packageName])
+wxs.addShortcut('workbenchshortcut', "Workbench", "Simulation apps are run through this front end", '[INSTALLDIR]bin\\workbench.exe', os.path.join(sourcePath, "symbole", 'logo_insight_cae.ico'))
+wxs.addShortcut('iscadshortcut', "isCAD", "A script-based CAD editor", '[INSTALLDIR]bin\\iscad.exe', os.path.join(sourcePath, "symbole", 'logo_insight_cae.ico'), False)
+wxs.addShortcut('isresulttoolshortcut', "Result Viewer", "A viewer for InsightCAE's ISR result files", '[INSTALLDIR]bin\\isResultTool.exe', os.path.join(sourcePath, "symbole", 'logo_insight_cae.ico'), False)
+
+wxs.write("insightcae.wxs")
+subprocess.run([os.path.join(sourcePath,"wixbuild.sh"), "insightcae"])
---
+++ generateNSIS.py
@@ -0,0 +1,108 @@
+#!/usr/bin/env python3
+# This Python file uses the following encoding: utf-8
+
+import os,sys,subprocess,re
+import urllib.request as req
+
+from optparse import OptionParser
+
+sourcePath = os.path.dirname(os.path.realpath(__file__))
+
+parser = OptionParser()
+
+parser.add_option("-c", "--customer", dest="customer", metavar='customer', default="ce",
+                  help="label of customer")
+
+parser.add_option("-p", "--password", dest="password", metavar='password', default="",
+                  help="password for customer repository")
+
+(opts, args) = parser.parse_args()
+
+
+installerfname="InsightCAEInstaller"
+if not (opts.customer=="ce" or opts.customer=="ce-dev"):
+    installerfname+="-"+opts.customer
+
+print("Generating installation package "+installerfname)
+
+class Dependency:
+    def __init__(self, URL=None, file=None):
+        if file is None:
+            if URL is None:
+                raise RuntimeError("URL and file must not be None at the same time.")
+            else:
+                self.file=URL.split('/')[-1]
+        else:
+            self.file=file
+        if not os.path.exists(self.file) and not URL is None:
+            req.urlretrieve(URL, self.file)
+
+    def config(self, label):
+        name,ext=os.path.splitext(os.path.basename(self.file))
+        return """
+Section "{label}"
+    File "/oname=$TEMP\{file}" "{file}"
+    {command}
+    Delete "$TEMP\{file}"
+SectionEnd
+""".format(
+    label=label,
+    file=self.file,
+    command=
+     "ExecWait 'msiexec /i \"$TEMP\{file}\"'".format(file=self.file) \
+     if ext=='msi' else \
+     'ExecShellWait "" "$TEMP\{file}"'.format(file=self.file)
+)
+
+
+putty=Dependency("https://the.earth.li/~sgtatham/putty/latest/w64/putty-64bit-0.76-installer.msi")
+gnuplot=Dependency("https://sourceforge.net/projects/gnuplot/files/gnuplot/5.2.8/gp528-win64-mingw.exe")
+miktex=Dependency("https://miktex.org/download/ctan/systems/win32/miktex/setup/windows-x64/basic-miktex-21.6-x64.exe")
+paraview=Dependency("https://www.paraview.org/paraview-downloads/download.php?submit=Download&version=v5.8&type=binary&os=Windows&downloadFile=ParaView-5.8.1-Windows-Python3.7-msvc2015-64bit.exe", "ParaView-5.8.1-Windows-Python3.7-msvc2015-64bit.exe")
+
+subprocess.call([
+ os.path.join(sourcePath, "generateInsightCAEWindowsMSI.py"),
+    "-c", opts.customer
+])
+insight=Dependency(file="insightcae.msi")
+
+subprocess.call([
+ os.path.join(sourcePath, "generateWSLMSI.py"),
+    "-c", opts.customer,
+    "-p", opts.password
+])
+insightwsl=Dependency(file="insightcae-wsl.msi")
+
+
+nsisScript=("""
+!include FileFunc.nsh
+!include LogicLib.nsh
+!include nsDialogs.nsh
+!include x64.nsh
+
+
+Name "InsightCAE on Windows"
+OutFile "{outFile}.exe"
+RequestExecutionLevel user
+LicenseData "{srcPath}/gpl.txt"
+Icon "{srcPath}/insightpackage.ico"
+
+Page license
+Page components
+Page instfiles
+
+""".format(
+    outFile=installerfname,
+    srcPath=sourcePath
+)) \
++putty.config("Putty") \
++gnuplot.config("Gnuplot") \
++miktex.config("MiKTeX") \
++paraview.config("ParaView 5.8") \
++insight.config("InsightCAE Windows Client and isCAD") \
++insightwsl.config("WSL Distibution (Ubuntu) with InsightCAE backend and OpenFOAM")
+
+nsisScriptfname=installerfname+".nsis"
+open(nsisScriptfname, 'w').write(nsisScript)
+
+subprocess.call(["i686-w64-mingw32.static-makensis", nsisScriptfname])

87fa93ad4ff2a8fd7c456a9a376997df1c2d3dbf
Hannes Kroeger <hannes@kroegeronline.net>
2022-06-19 23:21:29 +0200

    add prescribedvelocity restraint for rigidbodydynamics

---
+++ src/extensions/openfoam/sources/extendedRigidBodyDynamics/prescribedvelocity.cpp
@@ -0,0 +1,135 @@
+#include "prescribedvelocity.h"
+#include "addToRunTimeSelectionTable.H"
+
+namespace Foam {
+namespace RBD {
+namespace restraints {
+
+defineTypeNameAndDebug(prescribedVelocity, 0);
+
+addToRunTimeSelectionTable
+(
+    restraint,
+    prescribedVelocity,
+    dictionary
+);
+
+
+prescribedVelocity::prescribedVelocity
+(
+    const word& name,
+    const dictionary& dict,
+    const rigidBodyModel& model
+)
+:
+    restraint(name, dict, model),
+    velocitySet_(nullptr),
+    oldForce_(0),
+    error0_(0),
+    integral0_(0)
+{
+    read(dict);
+}
+
+
+prescribedVelocity::~prescribedVelocity()
+{}
+
+
+
+
+void prescribedVelocity::restrain
+(
+    scalarField& tau,
+    Field<spatialVector>& fx,
+    const rigidBodyModelState& state
+) const
+{
+
+    // Adding rotation to a given body
+    scalar velocity = model_.v(model_.master(bodyID_)).l() & direction_;
+
+    scalar Inertia = model_.I(model_.master(bodyID_)).m();
+
+    // from the definition of the angular momentum:
+    // force = m*ddt(x)
+
+    scalar t = model_.time().value();
+    scalar dt = model_.time().deltaTValue();
+
+    scalar error = velocitySet_->value(t) - velocity;
+    scalar integral = integral0_ + error;
+    scalar derivative = error - error0_;
+
+    scalar force = (p_*error + i_*integral + d_*derivative);
+    force *= Inertia/dt;
+
+    force = relax_*force + (1- relax_)*oldForce_;
+
+//    if (model_.debug)
+    {
+        Info<< " velocity  " << velocity << endl
+            << " wanted " << velocitySet_->value(t) << endl
+            << " force " << force << endl
+            << " inertia " << Inertia << endl
+            << " error " << error << endl
+            << " integral " << integral << endl
+            << " derivative " << derivative << endl
+            << " direction " << direction_
+            << endl;
+    }
+
+    // Accumulate the force for the restrained body
+    fx[bodyIndex_] += model_.X0(bodyID_).T() & spatialVector(Zero, force*direction_);
+
+    oldForce_ = force;
+    error0_ = error;
+    integral0_ = integral;
+}
+
+
+
+bool prescribedVelocity::read
+(
+    const dictionary& dict
+)
+{
+    restraint::read(dict);
+
+    direction_ = vector(coeffs_.lookup("direction"));
+
+    relax_=readScalar(coeffs_.lookup("relax"));
+
+    p_=readScalar(coeffs_.lookup("p"));
+    i_=readScalar(coeffs_.lookup("i"));
+    d_=readScalar(coeffs_.lookup("d"));
+
+    // Read the actual entry
+    velocitySet_.reset(Function1<scalar>::New("velocity", coeffs_, &model_.time()));
+
+    return true;
+}
+
+
+void prescribedVelocity::write
+(
+    Ostream& os
+) const
+{
+    restraint::write(os);
+
+    os << "direction" << token::SPACE << direction_ << token::END_STATEMENT << endl;
+    os << "relax" << token::SPACE << relax_ << token::END_STATEMENT << endl;
+    os << "p" << token::SPACE << p_ << token::END_STATEMENT << endl;
+    os << "i" << token::SPACE << i_ << token::END_STATEMENT << endl;
+    os << "d" << token::SPACE << d_ << token::END_STATEMENT << endl;
+
+    velocitySet_->writeData(os);
+}
+
+
+
+
+} // namespace restraints
+} // namespace RBD
+} // namespace Foam

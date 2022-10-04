b3b17bd773e2a915a34ef040cdb9d45c4d63f8bf
Hannes Kroeger <hannes@kroegeronline.net>
2022-10-04 10:01:58 +0200

    add enforceFarFieldVelocity frontend interface

---
+++ src/toolkit/openfoam/caseelements/options/enforcefarfieldvelocity.h
@@ -0,0 +1,45 @@
+#ifndef INSIGHT_ENFORCEFARFIELDVELOCITY_H
+#define INSIGHT_ENFORCEFARFIELDVELOCITY_H
+
+#include "openfoam/caseelements/openfoamcaseelement.h"
+
+#include "enforcefarfieldvelocity__EnforceFarFieldVelocity__Parameters_headers.h"
+
+namespace insight {
+
+class EnforceFarFieldVelocity
+        : public OpenFOAMCaseElement
+{
+public:
+
+#include "enforcefarfieldvelocity__EnforceFarFieldVelocity__Parameters.h"
+
+/*
+PARAMETERSET>>> EnforceFarFieldVelocity Parameters
+
+farFieldPatches = array [
+ string "" "patch name or regular expression (if set in quotes)"
+] *0 "selection of patches which make up the far field boundary"
+
+farFieldVelocityFieldName = string "zero" "name of the far field value. Reserved name 'zero' enforces a zero field value."
+
+transitionDistance = double 1 "distance within which the value is enforces"
+
+<<<PARAMETERSET
+*/
+
+protected:
+    Parameters p_;
+
+public:
+    declareType ( "EnforceFarFieldVelocity" );
+
+    EnforceFarFieldVelocity(OpenFOAMCase& c, const ParameterSet& p);
+
+    void addIntoDictionaries ( OFdicts& dictionaries ) const override;
+    static std::string category() { return "Tweaks"; }
+};
+
+} // namespace insight
+
+#endif // INSIGHT_ENFORCEFARFIELDVELOCITY_H

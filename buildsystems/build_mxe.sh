f67a216bc9c9836ee48f2cb94d6038626078e62f
Hannes Kroeger <hannes@kroegeronline.net>
2022-12-30 20:27:27 +0100

    update mxe

--- buildsystems/build_mxe.sh
+++ buildsystems/build_mxe.sh
@@ -4,5 +4,6 @@ export PATH=$PATH:/mxe/usr/bin
 
 cd /mxe
 
-make -j EXCLUDE_PKGS='librsvg db libsigrok check cryptopp gc gdal gdb gdk-pixbuf ghostscript libsoup upx wxwidgets pcl' MXE_TARGETS=i686-w64-mingw32.shared MXE_PLUGIN_DIRS='plugins/boost_1_66_0 plugins/gcc10'
-make -j EXCLUDE_PKGS='check cryptopp gdal ghostscript gst-plugins-bad imagemagick libsigrok ocaml% qtifw upx' MXE_TARGETS=i686-w64-mingw32.static MXE_PLUGIN_DIRS="plugins/gcc10 plugins/boost_1_66_0" nsis
+# no /opt/insightcae/python3.6 in path for meson!
+make -j MXE_TARGETS=i686-w64-mingw32.shared MXE_PLUGIN_DIRS='plugins/boost_1_66_0' armadillo boost dxflib freetype gcc libgcrypt glib gsl harfbuzz hdf5 libiconv libidn libidn2 vtk oce openssl jpeg qt5 cryptopp poppler libntlm openssl wt tiff libgsasl
+make -j MXE_TARGETS=i686-w64-mingw32.static MXE_PLUGIN_DIRS="plugins/boost_1_66_0" nsis

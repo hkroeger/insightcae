400e58c696f4a67ec7a75c3e96f30e277568f366
Hannes Kroeger <hannes@kroegeronline.net>
2023-06-06 09:17:18 +0200

    add compressibleInterFoam numerics and matching thermo props

---
+++ src/toolkit/openfoam/caseelements/basic/compressibletwophasethermophysicalproperties.h
@@ -0,0 +1,42 @@
+#ifndef INSIGHT_COMPRESSIBLETWOPHASETHERMOPHYSICALPROPERTIES_H
+#define INSIGHT_COMPRESSIBLETWOPHASETHERMOPHYSICALPROPERTIES_H
+
+#include "openfoam/caseelements/basic/transportmodel.h"
+#include "openfoam/caseelements/thermophysicalcaseelements.h"
+
+namespace insight {
+
+class compressibleTwoPhaseThermophysicalProperties
+    : public transportModel
+{
+
+public:
+#include "compressibletwophasethermophysicalproperties__compressibleTwoPhaseThermophysicalProperties__Parameters.h"
+/*
+PARAMETERSET>>> compressibleTwoPhaseThermophysicalProperties Parameters
+
+phases = array [
+     includedset "insight::SpeciesData::Parameters"
+    ] *2 "Definitions of the phases"
+
+sigma = double 0.07 "Surface tension"
+
+pMin = double 0 "[Pa] minimum pressure"
+
+<<<PARAMETERSET
+*/
+
+protected:
+    Parameters p_;
+
+public:
+    declareType ( "compressibleTwoPhaseThermophysicalProperties" );
+    compressibleTwoPhaseThermophysicalProperties ( OpenFOAMCase& c, const ParameterSet& ps = Parameters::makeDefault() );
+    void addIntoDictionaries ( OFdicts& dictionaries ) const override;
+
+    static std::string category() { return "Material Properties"; }
+};
+
+} // namespace insight
+
+#endif // INSIGHT_COMPRESSIBLETWOPHASETHERMOPHYSICALPROPERTIES_H
---
+++ src/toolkit/openfoam/caseelements/numerics/compressibleinterfoamnumerics.h
@@ -0,0 +1,39 @@
+#ifndef COMPRESSIBLEINTERFOAMNUMERICS_H
+#define COMPRESSIBLEINTERFOAMNUMERICS_H
+
+#include "openfoam/caseelements/numerics/interfoamnumerics.h"
+
+#include "compressibleinterfoamnumerics__compressibleInterFoamNumerics__Parameters_headers.h"
+
+namespace insight {
+
+class compressibleInterFoamNumerics
+    : public interFoamNumerics
+{
+
+public:
+#include "compressibleinterfoamnumerics__compressibleInterFoamNumerics__Parameters.h"
+    /*
+PARAMETERSET>>> compressibleInterFoamNumerics Parameters
+inherits interFoamNumerics::Parameters
+addTo_makeDefault { p.getDouble("pinternal")=1e5; }
+
+Tinternal = double 300.0 "Internal pressure field value"
+
+<<<PARAMETERSET
+*/
+
+protected:
+    Parameters p_;
+
+public:
+    declareType ( "compressibleInterFoamNumerics" );
+
+    compressibleInterFoamNumerics ( OpenFOAMCase& c, const ParameterSet& ps = Parameters::makeDefault() );
+    void addIntoDictionaries ( OFdicts& dictionaries ) const override;
+    bool isCompressible() const override;
+};
+
+}
+
+#endif // COMPRESSIBLEINTERFOAMNUMERICS_H

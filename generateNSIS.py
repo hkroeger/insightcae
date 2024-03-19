779c61bb1cc2da010e16cdbfc5ac0d18745bb925
Hannes Kroeger <hannes@kroegeronline.net>
2024-03-19 18:13:08 +0100

    fix syntax error

--- generateNSIS.py
+++ generateNSIS.py
@@ -89,7 +89,7 @@ class Dependency:
 Section "{label}"
     File "/oname=$TEMP\{file}" "{localfile}"
 """
-+(self.note is None?"":"MessageBox MB_OK \""+self.note"\"")
++("" if self.note is None else "MessageBox MB_OK \""+self.note"\"")
         +"""
     {command}
     Delete "$TEMP\{file}"

a2f537da33e25e166e4df6acaf590a019066788e
Hannes Kroeger <hannes@kroegeronline.net>
2024-07-08 21:27:09 +0200

    add convenience function

--- src/toolkit/base/rapidxml.cpp
+++ src/toolkit/base/rapidxml.cpp
@@ -2,7 +2,34 @@
 
 namespace insight {
 
-rapidxml::xml_node<>& appendNode(
+
+// OffspringNode::OffspringNode(
+//     rapidxml::xml_node<> &parentNode,
+//     rapidxml::xml_node<> &node )
+// : std::reference_wrapper<rapidxml::xml_node<> >(node),
+//   parentNode_(parentNode)
+// {}
+
+// OffspringNode::~OffspringNode()
+// {
+//     parentNode_.append_node ( &get() );
+// }
+
+std::reference_wrapper<rapidxml::xml_node<> >
+appendRootNode(
+    rapidxml::xml_document<> &doc,
+    const std::string &label)
+{
+    auto *newnode = doc.allocate_node(
+        rapidxml::node_element,
+        doc.allocate_string(label.c_str()) );
+    doc.append_node ( newnode );
+    return {*newnode};
+}
+
+
+std::reference_wrapper<rapidxml::xml_node<> >
+appendNode(
         rapidxml::xml_document<>& doc,
         rapidxml::xml_node<>& parent,
         const std::string& label )
@@ -11,7 +38,10 @@ rapidxml::xml_node<>& appendNode(
                 rapidxml::node_element,
                 doc.allocate_string(label.c_str()) );
     parent.append_node ( newnode );
-    return *newnode;
+    return {*newnode};
 }
+
+
+
 
 } // namespace insight
--- src/toolkit/base/rapidxml.h
+++ src/toolkit/base/rapidxml.h
@@ -9,6 +9,18 @@
 
 namespace insight {
 
+
+// class OffspringNode
+//     : public std::reference_wrapper<rapidxml::xml_node<> >
+// {
+//     rapidxml::xml_node<>& parentNode_;
+// public:
+//     OffspringNode(
+//         rapidxml::xml_node<>& parentNode,
+//         rapidxml::xml_node<>& node );
+//     ~OffspringNode();
+// };
+
 template<class Type>
 void appendAttribute(
         rapidxml::xml_document<>& doc,
@@ -27,8 +39,13 @@ void appendAttribute(
     );
 }
 
+std::reference_wrapper<rapidxml::xml_node<> >
+appendRootNode(
+    rapidxml::xml_document<>& doc,
+    const std::string& label );
 
-rapidxml::xml_node<>& appendNode(
+std::reference_wrapper<rapidxml::xml_node<> >
+appendNode(
         rapidxml::xml_document<>& doc,
         rapidxml::xml_node<>& parent,
         const std::string& label );

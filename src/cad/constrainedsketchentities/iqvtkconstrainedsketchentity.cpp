1360cdca35dcac1c643d4f76d2053a33808b6cea
Hannes Kroeger <hannes@kroegeronline.net>
2024-10-01 23:52:42 +0200

    repair target config export

--- src/cad/constrainedsketchentities/iqvtkconstrainedsketchentity.cpp
+++
@@ -1,7 +0,0 @@
-
-#include "constrainedsketchentities/iqvtkconstrainedsketchentity.h"
-
-IQVTKConstrainedSketchEntity::IQVTKConstrainedSketchEntity(
-    const std::string& layerName )
-    : insight::cad::ConstrainedSketchEntity(layerName)
-{}

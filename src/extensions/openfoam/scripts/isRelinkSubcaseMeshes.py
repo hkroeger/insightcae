e552f3af535f84415ac0a4b38430e7278cc5dcca
Hannes Kroeger <hannes@kroegeronline.net>
2025-03-24 15:14:19 +0100

    only relink, if argument is directory

--- src/extensions/openfoam/scripts/isRelinkSubcaseMeshes.py
+++ src/extensions/openfoam/scripts/isRelinkSubcaseMeshes.py
@@ -1,12 +1,14 @@
 #!/bin/bash
 
 for i in $*; do
- ( 
-  cd $i/constant/polyMesh;
-  for j in faces owner neighbour points; do
-   O=../../../constant/polyMesh/$j
-   if [ -e $O.gz ]; then ln -svf $O.gz .; fi
-   if [ -e $O ]; then ln -svf $O .; fi
-  done
+ (
+  if [ -d $i ]; then
+    cd $i/constant/polyMesh;
+    for j in faces owner neighbour points; do
+     O=../../../constant/polyMesh/$j
+     if [ -e $O.gz ]; then ln -svf $O.gz .; fi
+     if [ -e $O ]; then ln -svf $O .; fi
+    done
+  fi
  )
 done

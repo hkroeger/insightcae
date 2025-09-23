663f812d3a3d8e5dcc4c9b9a57447919688e0334
Hannes Kroeger <hannes@kroegeronline.net>
2025-09-23 11:06:05 +0200

    add for_each

--- src/toolkit/base/cppextensions.h
+++ src/toolkit/base/cppextensions.h
@@ -540,6 +540,71 @@ typename Map::const_iterator find_mapped_value(const Map& m, const typename Map:
 
 
 
+
+
+
+// the following is taken from sigidagi (https://stackoverflow.com/a/6401663)
+
+// ------------- UTILITY---------------
+template<int...> struct index_tuple{};
+
+template<int I, typename IndexTuple, typename... Types>
+struct make_indexes_impl;
+
+template<int I, int... Indexes, typename T, typename ... Types>
+struct make_indexes_impl<I, index_tuple<Indexes...>, T, Types...>
+{
+    typedef typename make_indexes_impl<I + 1, index_tuple<Indexes..., I>, Types...>::type type;
+};
+
+template<int I, int... Indexes>
+struct make_indexes_impl<I, index_tuple<Indexes...> >
+{
+    typedef index_tuple<Indexes...> type;
+};
+
+template<typename ... Types>
+struct make_indexes : make_indexes_impl<0, index_tuple<>, Types...>
+{};
+
+// ----------- FOR EACH -----------------
+template<typename Func, typename Last>
+void for_each_impl(Func&& f, Last&& last)
+{
+    f(last);
+}
+
+template<typename Func, typename First, typename ... Rest>
+void for_each_impl(Func&& f, First&& first, Rest&&...rest)
+{
+    f(first);
+    for_each_impl( std::forward<Func>(f), rest...);
+}
+
+template<typename Func, int ... Indexes, typename ... Args>
+void for_each_helper( Func&& f, index_tuple<Indexes...>, std::tuple<Args...>&& tup)
+{
+    for_each_impl( std::forward<Func>(f), std::forward<Args>(std::get<Indexes>(tup))...);
+}
+
+template<typename Func, typename ... Args>
+void for_each( std::tuple<Args...>& tup, Func&& f)
+{
+    for_each_helper(std::forward<Func>(f),
+                    typename make_indexes<Args...>::type(),
+                    std::forward<std::tuple<Args...>>(tup) );
+}
+
+template<typename Func, typename ... Args>
+void for_each( std::tuple<Args...>&& tup, Func&& f)
+{
+    for_each_helper(std::forward<Func>(f),
+                    typename make_indexes<Args...>::type(),
+                    std::forward<std::tuple<Args...>>(tup) );
+}
+
+
+
 }
 
 
@@ -561,6 +626,7 @@ public:
 
     std::vector<boost::signals2::shared_connection_block> block_all();
 };
+
 
 
 

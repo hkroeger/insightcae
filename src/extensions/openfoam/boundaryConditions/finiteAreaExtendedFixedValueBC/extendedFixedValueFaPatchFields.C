364a945410a3ea7e62abf196e01d3611f11ad9ce
Hannes Kroeger <hannes@kroegeronline.net>
2025-10-13 15:18:15 +0200

    add extendfixedvalue version for finitearea

---
+++ src/extensions/openfoam/boundaryConditions/finiteAreaExtendedFixedValueBC/extendedFixedValueFaPatchFields.C
@@ -0,0 +1,16 @@
+#include "extendedFixedValueFaPatchFields.H"
+#include "addToRunTimeSelectionTable.H"
+#include "areaFields.H"
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+namespace Foam
+{
+
+// * * * * * * * * * * * * * * Static Data Members * * * * * * * * * * * * * //
+
+makePatchFieldsE(extendedFixedValue);
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+} // End namespace Foam

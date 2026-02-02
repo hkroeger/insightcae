5919529e74f5a88e81fe95cfcfc4e282694d29f3
Hannes Kroeger <hannes@kroegeronline.net>
2026-02-02 15:52:54 +0100

    improve reorientation of background image

--- src/toolkit_gui/iqcadmodel3dviewer/iqvtkvieweractions/orientbackgroundimagecoordinatesdialog.h
+++ src/toolkit_gui/iqcadmodel3dviewer/iqvtkvieweractions/orientbackgroundimagecoordinatesdialog.h
@@ -14,11 +14,14 @@ class OrientBackgroundImageCoordinatesDialog : public QDialog
     Q_OBJECT
 
 public:
-    explicit OrientBackgroundImageCoordinatesDialog(const arma::mat& p1, const arma::mat& p2, QWidget *parent = nullptr);
+    explicit OrientBackgroundImageCoordinatesDialog(
+        const arma::mat& xy1, const arma::mat& xy2,
+        QWidget *parent = nullptr);
+
     ~OrientBackgroundImageCoordinatesDialog();
 
-    arma::mat p1() const;
-    arma::mat p2() const;
+    arma::mat xy1() const;
+    arma::mat xy2() const;
 
 private:
     Ui::OrientBackgroundImageCoordinatesDialog *ui;

b9cd58e859c2942a7fbecae71d62c0606e5eacd9
Hannes Kroeger <hannes@kroegeronline.net>
2026-06-15 17:48:59 +0200

    update submodels and introduce simplified dict access

--- src/toolkit/openfoam/caseelements/thermodynamics/thermalphasechangetwophasesystem.cpp
+++ src/toolkit/openfoam/caseelements/thermodynamics/thermalphasechangetwophasesystem.cpp
@@ -1,7 +1,226 @@
 #include "thermalphasechangetwophasesystem.h"
 #include "openfoam/caseelements/basic/thermodynamicmodel.h"
 
+#include "openfoam/ofdicts.h"
+
 namespace insight {
+
+namespace thermalPhaseChangeTwoPhaseSystemModels {
+
+
+defineType(diameterModel);
+defineDynamicClass(diameterModel);
+
+diameterModel::diameterModel(ParameterSetInput ip)
+    : p_(ip.forward<Parameters>())
+{}
+
+
+defineType(constantDiameter);
+addToFactoryTable(diameterModel, constantDiameter);
+addToStaticFunctionTable(diameterModel, constantDiameter, defaultParameters);
+
+
+constantDiameter::constantDiameter(ParameterSetInput ip)
+    :diameterModel(ip.forward<Parameters>())
+{}
+
+void constantDiameter::addIntoPhaseDict(OFDictData::dict& pd) const
+{
+    pd.merge_overwrite({
+        {"diameterModel", "constant"},
+        {"constantCoeffs", OFDictData::dict{
+                               {"d", p().d}
+                           }}
+    });
+}
+
+
+defineType(isothermalDiameter);
+addToFactoryTable(diameterModel, isothermalDiameter);
+addToStaticFunctionTable(diameterModel, isothermalDiameter, defaultParameters);
+
+
+isothermalDiameter::isothermalDiameter(ParameterSetInput ip)
+    :diameterModel(ip.forward<Parameters>())
+{}
+
+void isothermalDiameter::addIntoPhaseDict(OFDictData::dict& pd) const
+{
+    pd+={
+         {"diameterModel", "isothermal"},
+         {"isothermalCoeffs", OFDictData::dict{
+                                     {"d0", p().d0},
+                                     {"p0", p().p0}
+                                 }
+         }
+        };
+}
+
+
+
+
+defineType(phaseModel);
+defineDynamicClass(phaseModel);
+
+phaseModel::phaseModel(ParameterSetInput ip)
+    : p_(ip.forward<Parameters>())
+{}
+
+void phaseModel::addIntoPhaseDict(OFDictData::dict &pd) const
+{
+    pd+={
+            {"Sct", p().Sct},
+            {"residualAlpha", p().residualAlpha}
+        };
+}
+
+
+defineType(purePhase);
+addToFactoryTable(phaseModel, purePhase);
+addToStaticFunctionTable(phaseModel, purePhase, defaultParameters);
+
+
+purePhase::purePhase(ParameterSetInput ip)
+    :phaseModel(ip.forward<Parameters>())
+{}
+
+void purePhase::addIntoPhaseDict(OFDictData::dict& pd) const
+{
+    phaseModel::addIntoPhaseDict(pd);
+}
+
+
+
+defineType(dragModel);
+defineDynamicClass(dragModel);
+
+dragModel::dragModel(ParameterSetInput ip)
+    : p_(ip.forward<Parameters>())
+{}
+
+
+
+
+defineType(SchillerNaumannDrag);
+addToFactoryTable(dragModel, SchillerNaumannDrag);
+addToStaticFunctionTable(dragModel, SchillerNaumannDrag, defaultParameters);
+
+SchillerNaumannDrag::SchillerNaumannDrag(ParameterSetInput ip)
+    : dragModel(ip.forward<Parameters>())
+{}
+
+OFDictData::dict SchillerNaumannDrag::dragModelDict() const
+{
+    return {
+    };
+}
+
+
+
+
+
+
+defineType(blendingModel);
+defineDynamicClass(blendingModel);
+
+blendingModel::blendingModel(ParameterSetInput ip)
+    : p_(ip.forward<Parameters>())
+{}
+
+
+defineType(noBlending);
+addToFactoryTable(blendingModel, noBlending);
+addToStaticFunctionTable(blendingModel, noBlending, defaultParameters);
+
+noBlending::noBlending(ParameterSetInput ip)
+    : blendingModel(ip.forward<Parameters>())
+{}
+
+OFDictData::dict noBlending::blendingProperties() const
+{
+    return {
+        { "type", "none" },
+        { "continuousPhase", p().continuousPhase }
+    };
+}
+
+
+defineType(saturationModel);
+defineDynamicClass(saturationModel);
+
+saturationModel::saturationModel(ParameterSetInput ip)
+    : p_(ip.forward<Parameters>())
+{}
+
+
+
+defineType(TabulatedSaturation);
+addToFactoryTable(saturationModel, TabulatedSaturation);
+addToStaticFunctionTable(saturationModel, TabulatedSaturation, defaultParameters);
+
+TabulatedSaturation::TabulatedSaturation(ParameterSetInput ip)
+    : saturationModel(ip.forward<Parameters>())
+{}
+
+OFDictData::dict TabulatedSaturation::saturationModelDict() const
+{
+    return {
+    };
+}
+
+
+
+defineType(heatTransferModel);
+defineDynamicClass(heatTransferModel);
+
+heatTransferModel::heatTransferModel(ParameterSetInput ip)
+    : p_(ip.forward<Parameters>())
+{}
+
+
+defineType(sphericalHeatTransfer);
+addToFactoryTable(heatTransferModel, sphericalHeatTransfer);
+addToStaticFunctionTable(heatTransferModel, sphericalHeatTransfer, defaultParameters);
+
+sphericalHeatTransfer::sphericalHeatTransfer(ParameterSetInput ip)
+    : heatTransferModel(ip.forward<Parameters>())
+{}
+
+OFDictData::dict sphericalHeatTransfer::heatTransferModelDict() const
+{
+    return {
+    };
+}
+
+
+
+defineType(turbulentDispersionModel);
+defineDynamicClass(turbulentDispersionModel);
+
+turbulentDispersionModel::turbulentDispersionModel(ParameterSetInput ip)
+    : p_(ip.forward<Parameters>())
+{}
+
+
+defineType(constantCoefficient);
+addToFactoryTable(turbulentDispersionModel, constantCoefficient);
+addToStaticFunctionTable(turbulentDispersionModel, constantCoefficient, defaultParameters);
+
+constantCoefficient::constantCoefficient(ParameterSetInput ip)
+    : turbulentDispersionModel(ip.forward<Parameters>())
+{}
+
+OFDictData::dict constantCoefficient::turbulentDispersionModelDict() const
+{
+    return {
+    };
+}
+
+}
+
+
+
 
 defineType(thermalPhaseChangeTwoPhaseSystem);
 addToOpenFOAMCaseElementFactoryTable(thermalPhaseChangeTwoPhaseSystem);
@@ -12,7 +231,92 @@ thermalPhaseChangeTwoPhaseSystem::thermalPhaseChangeTwoPhaseSystem(OpenFOAMCase 
 
 void thermalPhaseChangeTwoPhaseSystem::addIntoDictionaries(OFdicts &dictionaries) const
 {
-    //OFDictData::dict& g=dictionaries.lookupDict("constant/g");
+    OFDictData::dict& pp=dictionaries.lookupDict("constant/phaseProperties");
+
+    pp+={
+        {"type", "thermalPhaseChangeTwoPhaseSystem"},
+        {"phaseChange", p().phaseChange},
+        {"pMin", p().pMin}
+    };
+
+    OFDictData::list phaseList;
+    for (auto &phase: p().phases)
+    {
+        phaseList.push_back(phase.first);
+        OFDictData::dict phaseDict;
+        phase.second->addIntoPhaseDict(phaseDict);
+        pp[phase.first]=phaseDict;
+    }
+
+    {
+        OFDictData::dict bd;
+        for (auto&bl: p().blending)
+        {
+            bd[bl.first]=bl.second->blendingProperties();
+        }
+        pp["blending"]=bd;
+    }
+
+    pp["surfaceTension"]=OFDictData::list();
+
+    pp["saturationModel"]=p().saturationModel->saturationModelDict();
+
+    pp["aspectRatio"]=OFDictData::list();
+
+    {
+        OFDictData::list dml;
+        for (auto &dm: p().drag)
+        {
+            dml.push_back(str(boost::format(
+                          "(%s in %s)"
+                          ) % dm.first % dm.second.inPhase
+                      ));
+            dml.push_back(
+                dm.second.model->dragModelDict()
+                );
+        }
+        pp["drag"]=dml;
+    }
+
+    {
+        std::map<std::string, OFDictData::list> htmls;
+        for (auto &htm: p().heatTransfer)
+        {
+            auto &html=htmls[htm.first];
+            html.push_back(str(boost::format(
+                                   "(%s in %s)"
+                                   ) % htm.second.inPhase % htm.first
+                               ));
+            html.push_back(
+                htm.second.model->heatTransferModelDict()
+                );
+        }
+        for (auto&html: htmls)
+        {
+            pp["heatTransfer."+html.first]=html.second;
+        }
+    }
+
+    pp["phaseTransfer"]=OFDictData::list();
+
+    pp["lift"]=OFDictData::list();
+
+    pp["wallLubrication"]=OFDictData::list();
+
+    {
+        OFDictData::list tdml;
+        for (auto &dm: p().turbulentDispersion)
+        {
+            tdml.push_back(str(boost::format(
+                                  "(%s in %s)"
+                                  ) % dm.first % dm.second.inPhase
+                              ));
+            tdml.push_back(
+                dm.second.model->turbulentDispersionModelDict()
+                );
+        }
+        pp["turbulentDispersion"]=tdml;
+    }
 }
 
 
--- src/toolkit/openfoam/caseelements/thermodynamics/thermalphasechangetwophasesystem.h
+++ src/toolkit/openfoam/caseelements/thermodynamics/thermalphasechangetwophasesystem.h
@@ -3,17 +3,352 @@
 
 #include "openfoam/caseelements/thermophysicalcaseelements.h"
 
-#include "thermalphasechangetwophasesystem__thermalPhaseChangeTwoPhaseSystem__Parameters_headers.h"
+#include "openfoam/openfoamdict.h"
+#include "thermalphasechangetwophasesystem_pdl.h"
 
 namespace insight {
+
+namespace thermalPhaseChangeTwoPhaseSystemModels {
+
+
+
+class diameterModel
+{
+public:
+#include THERMALPHASECHANGETWOPHASESYSTEM_PDL_diameterModel
+/*
+PARAMETERSET>>>
+createGetter
+<<<PARAMETERSET
+*/
+
+public:
+    declareType ( "diameterModel" );
+    declareDynamicClass(diameterModel);
+
+    diameterModel(ParameterSetInput ip = Parameters() );
+
+    virtual void addIntoPhaseDict ( OFDictData::dict& phaseDict ) const =0;
+};
+
+
+class constantDiameter : public diameterModel
+{
+public:
+#include THERMALPHASECHANGETWOPHASESYSTEM_PDL_constantDiameter
+/*
+PARAMETERSET>>>
+inherits diameterModel::Parameters
+
+d =double  0.00045 "[m] constant diameter"
+
+createGetter
+<<<PARAMETERSET
+*/
+public:
+    declareType ( "constant" );
+    constantDiameter(ParameterSetInput ip = Parameters() );
+
+    void addIntoPhaseDict ( OFDictData::dict& phaseDict ) const override;
+};
+
+
+
+class isothermalDiameter : public diameterModel
+{
+public:
+#include THERMALPHASECHANGETWOPHASESYSTEM_PDL_isothermalDiameter
+/*
+PARAMETERSET>>>
+inherits diameterModel::Parameters
+
+d0 =double  0.00045 "[m] reference diameter"
+p0 =double  1e5 "[Pa] reference pressure"
+
+createGetter
+<<<PARAMETERSET
+*/
+public:
+    declareType ( "isothermal" );
+    isothermalDiameter(ParameterSetInput ip = Parameters() );
+
+    void addIntoPhaseDict ( OFDictData::dict& phaseDict ) const override;
+};
+
+
+
+
+class phaseModel
+{
+public:
+#include THERMALPHASECHANGETWOPHASESYSTEM_PDL_phaseModel
+/*
+PARAMETERSET>>>
+
+diameter =  dynamicclassconfig "thermalPhaseChangeTwoPhaseSystemModels::diameterModel"
+        default "constant"
+        "The properties of the diameter model"
+
+Sct=double 0.7 "turbulent Schmidt number"
+
+residualAlpha = double 1e-4 ""
+
+createGetter
+<<<PARAMETERSET
+*/
+
+public:
+    declareType ( "phaseModel" );
+    declareDynamicClass(phaseModel);
+
+    phaseModel(ParameterSetInput ip = Parameters() );
+
+    virtual void addIntoPhaseDict ( OFDictData::dict& phaseDict ) const;
+};
+
+
+class purePhase : public phaseModel
+{
+public:
+#include THERMALPHASECHANGETWOPHASESYSTEM_PDL_purePhase
+/*
+PARAMETERSET>>>
+inherits phaseModel::Parameters
+
+createGetter
+<<<PARAMETERSET
+*/
+public:
+    declareType ( "purePhase" );
+    purePhase(ParameterSetInput ip = Parameters() );
+
+    void addIntoPhaseDict ( OFDictData::dict& phaseDict ) const override;
+};
+
+
+class dragModel
+{
+public:
+#include THERMALPHASECHANGETWOPHASESYSTEM_PDL_dragModel
+/*
+PARAMETERSET>>>
+
+residualRe = double 1e-3 ""
+
+createGetter
+<<<PARAMETERSET
+*/
+
+public:
+    declareType ( "dragModel" );
+    declareDynamicClass(dragModel);
+
+    dragModel(ParameterSetInput ip = Parameters() );
+
+    virtual OFDictData::dict dragModelDict() const =0;
+};
+
+
+
+
+class SchillerNaumannDrag : public dragModel
+{
+public:
+#include THERMALPHASECHANGETWOPHASESYSTEM_PDL_SchillerNaumannDrag
+/*
+PARAMETERSET>>>
+inherits dragModel::Parameters
+
+createGetter
+<<<PARAMETERSET
+*/
+public:
+    declareType ( "SchillerNaumann" );
+    SchillerNaumannDrag(ParameterSetInput ip = Parameters() );
+
+    OFDictData::dict dragModelDict() const override;
+};
+
+
+class blendingModel
+{
+public:
+#include THERMALPHASECHANGETWOPHASESYSTEM_PDL_blendingModel
+/*
+PARAMETERSET>>>
+createGetter
+<<<PARAMETERSET
+*/
+
+public:
+    declareType ( "blendingModel" );
+    declareDynamicClass(blendingModel);
+
+    blendingModel(ParameterSetInput ip = Parameters() );
+
+    virtual OFDictData::dict blendingProperties() const =0;
+};
+
+
+
+class noBlending : public blendingModel
+{
+public:
+#include THERMALPHASECHANGETWOPHASESYSTEM_PDL_noBlending
+/*
+PARAMETERSET>>>
+inherits blendingModel::Parameters
+
+continuousPhase = labeledArrayKeySelection "../../../phases" ""
+  "The name of the phase, which is not dispersed. The dispersed phases are the others."
+
+createGetter
+<<<PARAMETERSET
+*/
+    declareType ( "none" );
+    noBlending(ParameterSetInput ip = Parameters() );
+
+    OFDictData::dict blendingProperties() const override;
+};
+
+
+
+class saturationModel
+{
+public:
+#include THERMALPHASECHANGETWOPHASESYSTEM_PDL_saturationModel
+/*
+PARAMETERSET>>>
+createGetter
+<<<PARAMETERSET
+*/
+
+public:
+    declareType ( "saturationModel" );
+    declareDynamicClass(saturationModel);
+
+    saturationModel(ParameterSetInput ip = Parameters() );
+
+    virtual OFDictData::dict saturationModelDict() const =0;
+};
+
+
+
+class TabulatedSaturation : public saturationModel
+{
+public:
+#include THERMALPHASECHANGETWOPHASESYSTEM_PDL_TabulatedSaturation
+/*
+PARAMETERSET>>>
+inherits saturationModel::Parameters
+
+
+file = path "" "path to CSV file containing a table of saturation temperature vs. pressure"
+
+nHeaderLine         1;
+refColumn           0;
+#componentColumns    ;
+separator           =string "," ""
+mergeSeparators     = bool no ""
+
+createGetter
+<<<PARAMETERSET
+*/
+public:
+    declareType ( "CSVtabulated" );
+    TabulatedSaturation(ParameterSetInput ip = Parameters() );
+
+    OFDictData::dict saturationModelDict() const override;
+};
+
+
+
+class heatTransferModel
+{
+public:
+#include THERMALPHASECHANGETWOPHASESYSTEM_PDL_heatTransferModel
+/*
+PARAMETERSET>>>
+
+residualAlpha = double 1e-3 "residual phase fraction"
+
+createGetter
+<<<PARAMETERSET
+*/
+
+public:
+    declareType ( "heatTransferModel" );
+    declareDynamicClass(heatTransferModel);
+
+    heatTransferModel(ParameterSetInput ip = Parameters() );
+
+    virtual OFDictData::dict heatTransferModelDict() const =0;
+};
+
+
+class sphericalHeatTransfer : public heatTransferModel
+{
+
+public:
+    declareType ( "spherical" );
+    sphericalHeatTransfer(ParameterSetInput ip = Parameters() );
+
+    OFDictData::dict heatTransferModelDict() const override;
+};
+
+
+class turbulentDispersionModel
+{
+public:
+#include THERMALPHASECHANGETWOPHASESYSTEM_PDL_turbulentDispersionModel
+/*
+PARAMETERSET>>>
+residualAlpha = double 1e-3 "residual phase fraction"
+
+createGetter
+<<<PARAMETERSET
+*/
+
+public:
+    declareType ( "turbulentDispersionModel" );
+    declareDynamicClass(turbulentDispersionModel);
+
+    turbulentDispersionModel(ParameterSetInput ip = Parameters() );
+
+    virtual OFDictData::dict turbulentDispersionModelDict() const =0;
+};
+
+class constantCoefficient : public turbulentDispersionModel
+{
+public:
+#include THERMALPHASECHANGETWOPHASESYSTEM_PDL_constantCoefficient
+/*
+PARAMETERSET>>>
+inherits turbulentDispersionModel::Parameters
+
+Ctd = double 0.1 "dispersion coefficient"
+
+createGetter
+<<<PARAMETERSET
+*/
+public:
+    declareType ( "constantCoefficient" );
+    constantCoefficient(ParameterSetInput ip = Parameters() );
+
+    OFDictData::dict turbulentDispersionModelDict() const override;
+};
+
+}
+
+
 
 class thermalPhaseChangeTwoPhaseSystem
     : public thermodynamicModel
 {
 public:
-#include "thermalphasechangetwophasesystem__thermalPhaseChangeTwoPhaseSystem__Parameters.h"
+#include THERMALPHASECHANGETWOPHASESYSTEM_PDL_thermalPhaseChangeTwoPhaseSystem
 /*
-PARAMETERSET>>> thermalPhaseChangeTwoPhaseSystem Parameters
+PARAMETERSET>>>
 inherits OpenFOAMCaseElement::Parameters
 
 description
@@ -21,39 +356,49 @@ description
 
 phaseChange = bool true "Whether the phase change is allowed"
 
+
 phases = labeledarray "phase%d" [
-  set { }
+  dynamicclassconfig "thermalPhaseChangeTwoPhaseSystemModels::phaseModel"
+        default "purePhase"
+        "The properties of the phase of the system"
 ] *1 "phases in the system"
+
 
 drag = labeledarray keysFrom "phases" [ set {
  inPhase = labeledArrayKeySelection "../../phases" "" "the phase in which the drag is to be computed"
-} ] *0 "drag models"
-
-createGetter
-<<<PARAMETERSET
-*/
-
-    /*
-
-phases = labeledarray "phase%d" [
-  dynamicclassconfig "thermalPhaseChangeTwoPhaseSystem::PhaseModel" default "thermalPhaseChangeTwoPhaseSystem::purePhase"
-        "The properties of the phase of the system"
-] *1 "phases in the system"
-
-blending = array [
-  dynamicclassconfig "thermalPhaseChangeTwoPhaseSystem::blendingModel" default "thermalPhaseChangeTwoPhaseSystem::noBlending"
-        "The properties of the phase of the system"
-] *1 "blending models"
-
-drag = labeledarray keysFrom phases [ set {
- inPhase = selection ()
- model =  dynamicclassconfig "thermalPhaseChangeTwoPhaseSystem::dragModel" default "thermalPhaseChangeTwoPhaseSystem::SchillerNaumannDrag"
+ model =  dynamicclassconfig "thermalPhaseChangeTwoPhaseSystemModels::dragModel"
+        default "SchillerNaumann"
         "The properties of the drag model"
 } ] *0 "drag models"
 
+blending = labeledarray "blending%d" [
+  dynamicclassconfig "thermalPhaseChangeTwoPhaseSystemModels::blendingModel"
+        default "none"
+        "The properties of the phase blending"
+] *1 "blending models. This defines which phase is dispersed."
 
-saturationModel = dynamicclassconfig "saturationModel" default "CSV"
-            "The saturation curve"
+heatTransfer = labeledarray keysFrom "phases" [ set {
+ inPhase = labeledArrayKeySelection "../../phases" "" "the phase in which the heat transfer is to be computed"
+ model =  dynamicclassconfig "thermalPhaseChangeTwoPhaseSystemModels::heatTransferModel"
+        default "spherical"
+        "The properties of the heat transfer model model"
+} ] *0 "heat transfer models"
+
+turbulentDispersion = labeledarray keysFrom "phases" [ set {
+ inPhase = labeledArrayKeySelection "../../phases" "" "the phase in which the turbulent dispersion is to be computed"
+ model =  dynamicclassconfig "thermalPhaseChangeTwoPhaseSystemModels::turbulentDispersionModel"
+        default "constantCoefficient"
+        "The properties of the turbulent dispersion model"
+} ] *0 "turbulent dispersion models"
+
+pMin = double 1e4 "[Pa] Minimum pressure"
+
+saturationModel = dynamicclassconfig "thermalPhaseChangeTwoPhaseSystemModels::saturationModel"
+        default "CSVtabulated"
+        "The properties of the saturation curve"
+
+createGetter
+<<<PARAMETERSET
 */
 
 
